85b03a10b8291af70aa1d926c5e845a59a21037c
truemiller <mlxndrj@gmail.com>
2024-07-19 09:31:05 +0100

    fix: enable GH token to update to drafts

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ const publishOptions = {
   owner: 'valory-xyz',
   repo: 'olas-operate-app',
   releaseType: 'draft',
-  // token: process.env.GH_TOKEN,
+  token: process.env.GH_TOKEN,
   private: false,
   publishAutoUpdate: true,
 };
