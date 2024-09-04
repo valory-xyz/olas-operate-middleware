4de52d26f0feb1ca3cca37f3a66a0d056fcc9430
truemiller <mlxndrj@gmail.com>
2024-09-04 10:30:11 +0100

    fix: default contract fallback, instead of "not staked"

--- frontend/components/MainPage/sections/StakingContractUpdate.tsx
+++ frontend/components/MainPage/sections/StakingContractUpdate.tsx
@@ -1,6 +1,7 @@
 import { RightOutlined } from '@ant-design/icons';
 import { Button, Flex, Skeleton, Typography } from 'antd';
 
+import { STAKING_PROGRAM_META } from '@/constants/stakingProgramMeta';
 import { Pages } from '@/enums/PageState';
 import { usePageState } from '@/hooks/usePageState';
 import { useStakingProgram } from '@/hooks/useStakingProgram';
@@ -11,8 +12,11 @@ const { Text } = Typography;
 
 export const StakingContractUpdate = () => {
   const { goto } = usePageState();
-  const { activeStakingProgramMeta, isLoadedActiveStakingProgram } =
-    useStakingProgram();
+  const {
+    activeStakingProgramMeta,
+    isLoadedActiveStakingProgram,
+    defaultStakingProgram,
+  } = useStakingProgram();
 
   return (
     <CardSection bordertop="true" padding="16px 24px">
@@ -37,7 +41,7 @@ export const StakingContractUpdate = () => {
                 <RightOutlined />
               </>
             ) : (
-              'Not staked'
+              STAKING_PROGRAM_META[defaultStakingProgram].name
             )}
           </Button>
         ) : (
