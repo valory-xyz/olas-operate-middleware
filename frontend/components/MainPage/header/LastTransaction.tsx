8806c87aa2ea5360b08d34d6873daaba3ca5d79c
mohandast52 <mohandast52@gmail.com>
2024-09-13 05:19:54 +0530

    feat: add LastTransaction component

--- frontend/components/MainPage/header/LastTransaction.tsx
+++ frontend/components/MainPage/header/LastTransaction.tsx
@@ -1,17 +1,79 @@
-import { Typography } from 'antd';
+import { Skeleton, Typography } from 'antd';
+import { useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useInterval } from 'usehooks-ts';
+
+import { useAddress } from '@/hooks/useAddress';
+import { getLatestTransaction } from '@/service/Ethers';
+import { Transaction } from '@/types/Transaction';
+import { getTimeAgo } from '@/utils/time';
 
 const { Text } = Typography;
 
-/**
- * logic to improve activity
- * - Pool for the last transaction agent safe has made (TODO: agent safe or agent instance?)
- * - Update the time of the last transaction
- */
+const Loader = styled(Skeleton.Input)`
+  line-height: 1;
+  span {
+    width: 120px !important;
+    height: 12px !important;
+    margin-top: 6px !important;
+  }
+`;
 
+const POLLING_INTERVAL = 60 * 1000; // 1 minute
+
+/**
+ * TODO:
+ * - add cache if the same block number is requested
+ *
+ * Components to display the last transaction time and link to the transaction on GnosisScan
+ * by agent safe.
+ */
 export const LastTransaction = () => {
+  const { multisigAddress } = useAddress();
+
+  const [isFetching, setIsFetching] = useState(true);
+  const [transaction, setTransaction] = useState<Transaction | null>(null);
+
+  const fetchTransaction = useCallback(async () => {
+    if (!multisigAddress) return;
+
+    getLatestTransaction(`${process.env.GNOSIS_RPC}`, multisigAddress)
+      .then((tx) => setTransaction(tx))
+      .catch((error) =>
+        console.error('Failed to get latest transaction', error),
+      )
+      .finally(() => setIsFetching(false));
+  }, [multisigAddress]);
+
+  // Fetch the latest transaction on mount
+  useEffect(() => {
+    fetchTransaction();
+  }, [fetchTransaction]);
+
+  // Poll for the latest transaction
+  useInterval(() => fetchTransaction(), POLLING_INTERVAL);
+
+  if (isFetching) {
+    return <Loader active size="small" />;
+  }
+
+  if (!transaction) {
+    return (
+      <Text type="secondary" className="text-xs">
+        No transactions yet
+      </Text>
+    );
+  }
+
   return (
-    <Text type="secondary" className="text-xs">
-      Last txn: 15 mins ago ↗
+    <Text
+      type="secondary"
+      className="text-xs pointer"
+      onClick={() =>
+        window.open(`https://gnosisscan.io/tx/${transaction.hash}`)
+      }
+    >
+      Last txn: {getTimeAgo(transaction.timestamp)} ↗
     </Text>
   );
 };
