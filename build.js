1f3d8a602f608d55be5ab488f7c9973371222dd1
truemiller <mlxndrj@gmail.com>
2024-09-16 17:03:29 +0100

    feat: caching, parallelization, generated notes

--- build.js
+++ build.js
@@ -46,8 +46,8 @@ const main = async () => {
       mac: {
         target: [
           {
-            target: 'default',
-            arch: ['x64', 'arm64'],
+            target: 'dmg',
+            arch: [process.env.ARCH],
           },
         ],
         publish: publishOptions,
