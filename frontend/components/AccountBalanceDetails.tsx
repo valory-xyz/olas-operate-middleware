d4b6bf7df17ab0ad80eebea2f570b9b47e02f66e
mohandast52 <mohandast52@gmail.com>
2024-09-18 13:16:56 +0530

    feat: add dummy XdaoBalance in  AccountBalanceDetails component

--- frontend/components/AccountBalanceDetails.tsx
+++ frontend/components/AccountBalanceDetails.tsx
@@ -10,6 +10,9 @@ import { CustomModal } from './styled/CustomModal';
 const { Title } = Typography;
 
 const Container = styled.div`
+  > div:not(:last-child) {
+    margin-bottom: 16px;
+  }
   .ant-card-body {
     /* display: flex; */
     /* flex-direction: column; */
@@ -18,9 +21,9 @@ const Container = styled.div`
   }
 `;
 
-export const AccountBalanceDetails = () => {
-  const [isModalVisible, setIsModalVisible] = useState(true);
+const infoBreakdownParentStyle = { gap: 8 };
 
+const OlasBalance = () => {
   const olasBalances = useMemo(() => {
     return [
       {
@@ -35,6 +38,55 @@ export const AccountBalanceDetails = () => {
   }, []);
 
   return (
+    <Card>
+      <Flex vertical gap={8}>
+        <Title level={5} className="m-0">
+          OLAS
+        </Title>
+        <InfoBreakdownList
+          list={olasBalances.map((item) => ({
+            left: item.title,
+            right: `${item.value} OLAS`,
+          }))}
+          parentStyle={infoBreakdownParentStyle}
+        />
+      </Flex>
+    </Card>
+  );
+};
+
+const XdaiBalance = () => {
+  const olasBalances = useMemo(() => {
+    return [
+      {
+        title: 'Available',
+        value: '100',
+      },
+    ];
+  }, []);
+
+  return (
+    <Card>
+      <Flex vertical gap={8}>
+        <Title level={5} className="m-0">
+          XDAI
+        </Title>
+        <InfoBreakdownList
+          list={olasBalances.map((item) => ({
+            left: item.title,
+            right: `${item.value} XDAI`,
+          }))}
+          parentStyle={infoBreakdownParentStyle}
+        />
+      </Flex>
+    </Card>
+  );
+};
+
+export const AccountBalanceDetails = () => {
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  return (
     <CustomModal
       title="Account balance details"
       open={isModalVisible}
@@ -44,20 +96,8 @@ export const AccountBalanceDetails = () => {
       footer={null}
     >
       <Container>
-        <Card>
-          <Flex vertical gap={8}>
-            <Title level={5} className="m-0">
-              OLAS
-            </Title>
-            <InfoBreakdownList
-              list={olasBalances.map((item) => ({
-                left: item.title,
-                right: `${item.value} OLAS`,
-              }))}
-              parentStyle={{ padding: 4, gap: 8 }}
-            />
-          </Flex>
-        </Card>
+        <OlasBalance />
+        <XdaiBalance />
       </Container>
     </CustomModal>
   );
