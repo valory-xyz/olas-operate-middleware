689db82731a51ec4b6ef6a93f53c3f2358ef75e7
mohandast52 <mohandast52@gmail.com>
2024-09-18 13:16:39 +0530

    feat: add dummy OlasBalance in  AccountBalanceDetails component

--- frontend/components/AccountBalanceDetails.tsx
+++ frontend/components/AccountBalanceDetails.tsx
@@ -1,22 +1,64 @@
-import { useState } from 'react';
+import { Card, Flex, Typography } from 'antd';
+import { useMemo, useState } from 'react';
+import styled from 'styled-components';
 
 import { MODAL_WIDTH } from '@/constants/width';
 
+import { InfoBreakdownList } from './InfoBreakdown';
 import { CustomModal } from './styled/CustomModal';
+
+const { Title } = Typography;
+
+const Container = styled.div`
+  .ant-card-body {
+    /* display: flex; */
+    /* flex-direction: column; */
+    /* gap: 8px; */
+    padding: 16px;
+  }
+`;
 
 export const AccountBalanceDetails = () => {
   const [isModalVisible, setIsModalVisible] = useState(true);
 
+  const olasBalances = useMemo(() => {
+    return [
+      {
+        title: 'Available',
+        value: '100',
+      },
+      {
+        title: 'Staked',
+        value: '200',
+      },
+    ];
+  }, []);
+
   return (
     <CustomModal
-      title="Account Balance Details"
+      title="Account balance details"
       open={isModalVisible}
       width={MODAL_WIDTH}
       bodyPadding
       onCancel={() => setIsModalVisible(false)}
       footer={null}
     >
-      <div>Some contents...</div>
+      <Container>
+        <Card>
+          <Flex vertical gap={8}>
+            <Title level={5} className="m-0">
+              OLAS
+            </Title>
+            <InfoBreakdownList
+              list={olasBalances.map((item) => ({
+                left: item.title,
+                right: `${item.value} OLAS`,
+              }))}
+              parentStyle={{ padding: 4, gap: 8 }}
+            />
+          </Flex>
+        </Card>
+      </Container>
     </CustomModal>
   );
 };
