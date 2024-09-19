e44e9b304d07e7727d11b37f44c4f3fadf40ab97
mohandast52 <mohandast52@gmail.com>
2024-09-19 17:31:50 +0530

    feat: Remove current balance tooltip

--- frontend/components/MainPage/sections/OlasBalanceSection.tsx
+++ frontend/components/MainPage/sections/OlasBalanceSection.tsx
@@ -1,15 +1,13 @@
-import { InfoCircleOutlined, WalletOutlined } from '@ant-design/icons';
-import { Button, Flex, Skeleton, Tooltip, Typography } from 'antd';
+import { WalletOutlined } from '@ant-design/icons';
+import { Button, Flex, Skeleton, Typography } from 'antd';
 import { useMemo, useState } from 'react';
 import styled from 'styled-components';
 
 import { CustomAlert } from '@/components/Alert';
-import { InfoBreakdownList } from '@/components/InfoBreakdown';
 import { UNICODE_SYMBOLS } from '@/constants/symbols';
 import { LOW_MASTER_SAFE_BALANCE } from '@/constants/thresholds';
 import { useBalance } from '@/hooks/useBalance';
 import { useElectronApi } from '@/hooks/useElectronApi';
-import { useReward } from '@/hooks/useReward';
 import { useStore } from '@/hooks/useStore';
 import { balanceFormat } from '@/utils/numberFormatters';
 
@@ -23,59 +21,6 @@ const Balance = styled.span`
   letter-spacing: -2px;
   margin-right: 4px;
 `;
-
-const OVERLAY_STYLE = { maxWidth: '300px', width: '300px' };
-
-const CurrentBalance = () => {
-  const { totalOlasBalance, totalOlasStakedBalance } = useBalance();
-  const { accruedServiceStakingRewards } = useReward();
-
-  const balances = useMemo(() => {
-    return [
-      {
-        title: 'Staked amount',
-        value: balanceFormat(totalOlasStakedBalance ?? 0, 2),
-      },
-      {
-        title: 'Unclaimed rewards',
-        value: balanceFormat(accruedServiceStakingRewards ?? 0, 2),
-      },
-      {
-        // Unused funds should only be ‘free-floating’ OLAS that is neither unclaimed nor staked.
-        title: 'Unused funds',
-        value: balanceFormat(
-          (totalOlasBalance ?? 0) -
-            (totalOlasStakedBalance ?? 0) -
-            (accruedServiceStakingRewards ?? 0),
-          2,
-        ),
-      },
-    ];
-  }, [accruedServiceStakingRewards, totalOlasBalance, totalOlasStakedBalance]);
-
-  return (
-    <Text type="secondary">
-      Current balance&nbsp;
-      <Tooltip
-        arrow={false}
-        placement="bottom"
-        overlayStyle={OVERLAY_STYLE}
-        title={
-          <InfoBreakdownList
-            list={balances.map((item) => ({
-              left: item.title,
-              right: `${item.value} OLAS`,
-            }))}
-            size="small"
-            parentStyle={{ padding: 4, gap: 8 }}
-          />
-        }
-      >
-        <InfoCircleOutlined />
-      </Tooltip>
-    </Text>
-  );
-};
 
 const MainOlasBalanceAlert = styled.div`
   .ant-alert {
@@ -197,7 +142,7 @@ export const MainOlasBalance = () => {
       {isBalanceLoaded ? (
         <Flex className="w-full" align="center" justify="space-between">
           <Flex vertical gap={8}>
-            <CurrentBalance />
+            <Text type="secondary">Current balance</Text>
             <Flex align="end">
               <span className="balance-symbol">{UNICODE_SYMBOLS.OLAS}</span>
               <Balance className="balance">{balance}</Balance>
