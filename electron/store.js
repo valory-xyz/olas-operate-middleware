daaebfa44d2a2c96c43784472e638408a2b0130b
truemiller <mlxndrj@gmail.com>
2024-09-24 18:25:33 +0100

    feat: update store and add version based migrations

--- electron/store.js
+++ electron/store.js
@@ -1,13 +1,40 @@
 const Store = require('electron-store');
-// set schema to validate store data
+
+const { logger } = require('./logger');
+
+/**
+ * The schema for the Electron store.
+ * Uses JSON Schema format to define the types and defaults for the store.
+ * @type {Store.Schema<{[key: string]: unknown}>}
+ */
 const schema = {
-  isInitialFunded: { type: 'boolean', default: false }, // TODO: reconsider this default, can be problematic if user has already funded prior to implementation
+  isInitialFunded: { type: ['boolean', 'null'], default: null },
   firstStakingRewardAchieved: { type: 'boolean', default: false },
   firstRewardNotificationShown: { type: 'boolean', default: false },
   agentEvictionAlertShown: { type: 'boolean', default: false },
+};
 
-  environmentName: { type: 'string', default: '' },
-  currentStakingProgram: { type: 'string', default: '' },
+/**
+ * Migrations for the Electron store.
+ * Each migration function should take the store as an argument and update it in place.
+ * Migrations are run in order, starting from the oldest version and updating to the latest.
+ * @note The migration function should be named after the version it
+ * migrates to, e.g. '0.1.0-rc143' for version 0.1.0-rc143.
+ * @note All versions prior will be migrated to the latest version.
+ * @type {Record<string, (store: Store<{[key: string]: unknown}>) => void>} */
+const migrations = {
+  '0.1.0-rc143': (store) => {
+    // Environment name and current staking program are no longer stored
+    if (store.has('environmentName')) {
+      logger.electron('Removing environmentName from store');
+      store.delete('environmentName');
+    }
+    // Remove the current staking program, was never implemented
+    if (store.has('currentStakingProgram')) {
+      logger.electron('Removing currentStakingProgram from store');
+      store.delete('currentStakingProgram');
+    }
+  },
 };
 
 /**
@@ -16,8 +43,17 @@ const schema = {
  * @param {Electron.BrowserWindow} mainWindow - The main Electron browser window.
  * @returns {Promise<void>} - A promise that resolves once the store is set up.
  */
-const setupStoreIpc = async (ipcMain, mainWindow) => {
-  const store = new Store({ schema });
+const setupStore = async (ipcMain, mainWindow) => {
+  const store = new Store({
+    schema,
+    migrations,
+    beforeEachMigration: (_, context) => {
+      // Log the migration version before it runs
+      logger.electron(
+        `Migrating store from ${context.fromVersion} to ${context.toVersion}`,
+      );
+    },
+  });
 
   store.onDidAnyChange((data) => {
     if (mainWindow?.webContents)
@@ -32,4 +68,4 @@ const setupStoreIpc = async (ipcMain, mainWindow) => {
   ipcMain.handle('store-clear', (_) => store.clear());
 };
 
-module.exports = { setupStoreIpc };
+module.exports = { setupStore };
