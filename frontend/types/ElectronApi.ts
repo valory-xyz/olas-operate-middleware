687869c221b8a4e59b50ddeb3cce639917a99189
truemiller <mlxndrj@gmail.com>
2024-09-25 23:38:15 +0100

    feat: added recursive type function for nested ipc functions

--- frontend/types/ElectronApi.ts
+++ frontend/types/ElectronApi.ts
@@ -1,9 +1,9 @@
 export type ElectronStore = {
-  environmentName?: string;
   isInitialFunded?: boolean;
   firstStakingRewardAchieved?: boolean;
   firstRewardNotificationShown?: boolean;
   agentEvictionAlertShown?: boolean;
+  canCheckForUpdates?: boolean | null;
 };
 
 export type ElectronTrayIconStatus =
@@ -11,3 +11,12 @@ export type ElectronTrayIconStatus =
   | 'running'
   | 'paused'
   | 'logged-out';
+
+/**
+ * Utility type for applying types to ElectronAPI object's properties recursively.*/
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- this is a generic type, `any` is acceptable
+export type RecursiveFunction<T> = T extends (...args: any[]) => any
+  ? T
+  : {
+      [K in keyof T]: RecursiveFunction<T[K]>;
+    };
