4edd933e5249f15a725dd4db36bf7b77f7d4ebc1
mohandast52 <mohandast52@gmail.com>
2024-09-27 05:50:23 +0530

    feat: add actual query (used in studio)

--- frontend/components/RewardsHistory/useRewardsHistory.ts
+++ frontend/components/RewardsHistory/useRewardsHistory.ts
@@ -28,6 +28,34 @@ const beta2Address =
 const SUBGRAPH_URL =
   'https://api.studio.thegraph.com/query/81855/pearl-staking-rewards-history/version/latest';
 
+/*
+// actual one
+  {
+    beta2Rewards: checkpoints(
+      first: 1
+      orderBy: epoch
+      orderDirection: desc
+      where: { contractAddress: ${beta2Address} }
+    ) {
+      epoch
+      rewards
+      serviceIds
+      blockTimestamp
+    }
+    betaRewards: checkpoints(
+      first: 1
+      orderBy: epoch
+      orderDirection: desc
+      where: { contractAddress: ${betaAddress} }
+    ) {
+      epoch
+      rewards
+      serviceIds
+      blockTimestamp
+    }
+  }
+*/
+
 const fetchRewardsQuery = gql`
   {
     allRewards: checkpoints(orderBy: epoch, orderDirection: desc) {
@@ -86,16 +114,8 @@ export const useRewardsHistory = () => {
         (data as { allRewards: RewardHistoryResponse[] }).allRewards,
         'contractAddress',
       );
-      // const [betaRewards, beta2Rewards] = Object.values(allRewards);
       const betaRewards = allRewards[betaAddress.toLowerCase()];
       const beta2Rewards = allRewards[beta2Address.toLowerCase()];
-
-      // empty array
-      // const betaRewards = [] as RewardHistoryResponse[];
-      // const beta2Rewards = [] as RewardHistoryResponse[];
-
-      // const beta2Rewards = (data as { allRewards: RewardHistoryResponse[] })
-      //   .allRewards;
 
       return [
         {
