924306e38b066368c1d0881ee02cefec5c6636d0
truemiller <mlxndrj@gmail.com>
2024-09-29 23:40:31 +0100

    release: rc149

--- electron/store.js
+++ electron/store.js
@@ -23,7 +23,7 @@ const schema = {
  * @note All versions prior will be migrated to the latest version.
  * @type {Record<string, (store: Store<{[key: string]: unknown}>) => void>} */
 const migrations = {
-  '0.1.0-rc150': (store) => {
+  '0.1.0-rc149': (store) => {
     // Environment name and current staking program are unused
     // can revisit environment name if we need to support multiple environments
     if (store.has('environmentName')) {
