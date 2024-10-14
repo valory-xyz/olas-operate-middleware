611459b8d3b82a07f89cd380fd6d0eb0863c3b4d
truemiller <mlxndrj@gmail.com>
2024-10-14 17:42:09 +0100

    feat: add rewards streak box

---
+++ frontend/components/MainPage/sections/RewardsSection/RewardsStreak.tsx
@@ -0,0 +1,58 @@
+import { RightOutlined } from '@ant-design/icons';
+import { Flex, Typography } from 'antd';
+import { useMemo } from 'react';
+import styled from 'styled-components';
+
+import { FireNoStreak } from '@/components/custom-icons/FireNoStreak';
+import { FireStreak } from '@/components/custom-icons/FireStreak';
+import { Pages } from '@/enums/PageState';
+import { usePageState } from '@/hooks/usePageState';
+import { useRewardsHistory } from '@/hooks/useRewardsHistory';
+
+const { Text } = Typography;
+
+const RewardsStreakFlex = styled(Flex)`
+  padding: 8px 16px;
+  background: #f2f4f9;
+  border-radius: 6px;
+  justify-content: space-between;
+  height: 40px;
+  align-items: center;
+`;
+
+const StreakIcon = ({ isOnStreak }: { isOnStreak: boolean }) => {
+  if (isOnStreak) return <FireStreak />;
+  return <FireNoStreak />;
+};
+const StreakText = ({ streak }: { streak: number }) => {
+  if (streak > 0) return <div>{streak} day streak</div>;
+  return <div>No streak</div>;
+};
+
+export const RewardsStreak = () => {
+  const { goto } = usePageState();
+  const { rewards } = useRewardsHistory();
+
+  const streak = useMemo(() => {
+    if (!rewards) return 0;
+    return 0;
+  }, [rewards]);
+
+  return (
+    <RewardsStreakFlex>
+      <span style={{ display: 'inline-flex', gap: 8 }}>
+        <StreakIcon isOnStreak={!!streak} />
+        <StreakText streak={streak} />
+      </span>
+
+      <Text
+        type="secondary"
+        className="text-sm pointer hover-underline"
+        onClick={() => goto(Pages.RewardsHistory)}
+      >
+        See rewards history
+        <RightOutlined style={{ fontSize: 12, paddingLeft: 6 }} />
+      </Text>
+    </RewardsStreakFlex>
+  );
+};
