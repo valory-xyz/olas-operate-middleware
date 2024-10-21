949f33cc6b32adf23c2746cb4960ce20ae86f5cf
truemiller <mlxndrj@gmail.com>
2024-10-21 16:21:27 +0100

    chore: rename to index

--- frontend/components/YourWalletPage/index.tsx
+++ frontend/components/YourWalletPage/index.tsx
@@ -16,6 +16,7 @@ import { CardFlex } from '@/components/styled/CardFlex';
 import { Pages } from '@/enums/PageState';
 import { useBalance } from '@/hooks/useBalance';
 import { usePageState } from '@/hooks/usePageState';
+import { useServices } from '@/hooks/useServices';
 import { useWallet } from '@/hooks/useWallet';
 import { balanceFormat } from '@/utils/numberFormatters';
 
@@ -127,8 +128,9 @@ const Signer = () => {
   );
 };
 
-export const YourWallet = () => {
+export const YourWalletPage = () => {
   const { goto } = usePageState();
+  const { service } = useServices();
 
   return (
     <ConfigProvider theme={yourWalletTheme}>
@@ -149,7 +151,7 @@ export const YourWallet = () => {
             <OlasBalance />
             <XdaiBalance />
             <Signer />
-            <YourAgentWallet />
+            {service && <YourAgentWallet />}
           </Container>
         </Card>
       </CardFlex>
