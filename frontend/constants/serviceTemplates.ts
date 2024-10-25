f2cea2577e12d9aa7c22b1897d090bbe04100304
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2024-10-25 15:27:52 +0200

    chore: update hash

--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -4,7 +4,7 @@ import { StakingProgramId } from '@/enums/StakingProgram';
 export const SERVICE_TEMPLATES: ServiceTemplate[] = [
   {
     name: 'Trader Agent',
-    hash: 'bafybeibvnr4oe2gqi2aq4ledf4uspop76u4ruzsfw2iob342supm4dy7pu',
+    hash: 'bafybeiax7tk4egt5cbcfzchanhpn4q6expi4rax3mzezaz3sec5hmnpvey',
     description: 'Trader agent for omen prediction markets',
     image:
       'https://operate.olas.network/_next/image?url=%2Fimages%2Fprediction-agent.png&w=3840&q=75',
