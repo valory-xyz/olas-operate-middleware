3535557c22aebad867a13e5a04e81ba94d1abdbf
truemiller <mlxndrj@gmail.com>
2024-10-31 13:38:56 +0000

    feat: add optimism provider

--- frontend/constants/providers.ts
+++ frontend/constants/providers.ts
@@ -1,8 +1,22 @@
 import { ethers } from 'ethers';
 import { Provider } from 'ethers-multicall';
 
+import { CHAINS } from './chains';
+
 export const gnosisProvider = new ethers.providers.StaticJsonRpcProvider(
   process.env.GNOSIS_RPC,
 );
 
-export const gnosisMulticallProvider = new Provider(gnosisProvider, 100);
+export const gnosisMulticallProvider = new Provider(
+  gnosisProvider,
+  CHAINS.GNOSIS.chainId,
+);
+
+export const optimismProvider = new ethers.providers.StaticJsonRpcProvider(
+  process.env.OPTIMISM_RPC,
+);
+
+export const optimismMulticallProvider = new Provider(
+  optimismProvider,
+  CHAINS.OPTIMISM.chainId,
+);
