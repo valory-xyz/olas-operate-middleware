580e7fb20cf5bedf013b46750384e21310519c5c
truemiller <mlxndrj@gmail.com>
2024-11-01 03:31:23 +0000

    fix: pre-initialize providers with chainIds

--- frontend/constants/providers.ts
+++ frontend/constants/providers.ts
@@ -1,40 +1,56 @@
 import { ethers } from 'ethers';
-import { Provider } from 'ethers-multicall';
+import { Provider as MulticallProvider } from 'ethers-multicall';
 
 import { CHAINS } from './chains';
 
-// export const gnosisProvider = new ethers.providers.StaticJsonRpcProvider(
-//   process.env.RPC,
-// );
-
-// export const gnosisMulticallProvider = new Provider(
-//   gnosisProvider,
-//   CHAINS.GNOSIS.chainId,
-// );
-
-export const optimismProvider = new ethers.providers.StaticJsonRpcProvider(
-  process.env.OPTIMISM_RPC,
+export const gnosisProvider = new ethers.providers.JsonRpcProvider(
+  process.env.RPC,
+  {
+    chainId: CHAINS.GNOSIS.chainId,
+    name: CHAINS.GNOSIS.name,
+  },
 );
 
-export const optimismMulticallProvider = new Provider(
+export const gnosisMulticallProvider = new MulticallProvider(
+  gnosisProvider,
+  CHAINS.GNOSIS.chainId,
+);
+
+export const optimismProvider = new ethers.providers.JsonRpcProvider(
+  process.env.OPTIMISM_RPC,
+  {
+    chainId: CHAINS.OPTIMISM.chainId,
+    name: CHAINS.OPTIMISM.name,
+  },
+);
+
+export const optimismMulticallProvider = new MulticallProvider(
   optimismProvider,
   CHAINS.OPTIMISM.chainId,
 );
 
-export const ethereumProvider = new ethers.providers.StaticJsonRpcProvider(
+export const ethereumProvider = new ethers.providers.JsonRpcProvider(
   process.env.ETHEREUM_RPC,
+  {
+    chainId: CHAINS.ETHEREUM.chainId,
+    name: CHAINS.ETHEREUM.name,
+  },
 );
 
-export const ethereumMulticallProvider = new Provider(
+export const ethereumMulticallProvider = new MulticallProvider(
   ethereumProvider,
   CHAINS.ETHEREUM.chainId,
 );
 
-export const baseProvider = new ethers.providers.StaticJsonRpcProvider(
+export const baseProvider = new ethers.providers.JsonRpcProvider(
   process.env.BASE_RPC,
+  {
+    chainId: CHAINS.BASE.chainId,
+    name: CHAINS.BASE.name,
+  },
 );
 
-export const baseMulticallProvider = new Provider(
+export const baseMulticallProvider = new MulticallProvider(
   baseProvider,
   CHAINS.BASE.chainId,
 );
