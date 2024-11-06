dfbba88264039e6d017591afdfebba740224e563
mohandast52 <mohandast52@gmail.com>
2024-11-06 19:02:40 +0530

    feat: add function to get staking program id by contract address

--- frontend/utils/service.ts
+++ frontend/utils/service.ts
@@ -1,4 +1,5 @@
 import { ServiceTemplate } from '@/client';
+import { SERVICE_STAKING_CONTRACT_ADDRESSES } from '@/constants/contractAddresses';
 import { DEFAULT_STAKING_PROGRAM_ID } from '@/context/StakingProgramProvider';
 import { StakingProgramId } from '@/enums/StakingProgram';
 
@@ -36,4 +37,22 @@ export const getMinimumStakedAmountRequired = (
   }
 
   return;
+};
+
+/**
+ *
+ * Get the staking program id by address
+ * @example getStakingProgramIdByAddress('0x3052451e1eAee78e62E169AfdF6288F8791F2918') // StakingProgramId.Beta4
+ */
+export const getStakingProgramIdByAddress = (
+  contractAddress: `0x${string}`,
+): StakingProgramId | undefined => {
+  const entries = Object.entries(SERVICE_STAKING_CONTRACT_ADDRESSES) as [
+    StakingProgramId,
+    `0x${string}`,
+  ][];
+  const foundEntry = entries.find(
+    ([, address]) => address.toLowerCase() === contractAddress.toLowerCase(),
+  );
+  return foundEntry ? foundEntry[0] : undefined;
 };
