404b600899ed838a3536988d3cf90029b14fbe30
Mohan <mohandast52@gmail.com>
2024-11-08 21:12:58 +0530

    fix: issue with streak being stuck and include new staking contracts (#424)
    * feat: tiny cleanup
    
    * feat: add function to get staking program id by contract address
    
    * feat: add staking contract addresses and update rewards history to include contract name
    
    * feat: enhance RewardsStreak component with loading state and improve rewards history hook logic
    
    * feat: refactor RewardsStreak component to handle loading state and improve streak calculation logic
    
    * feat: update useRewardsHistory hook to improve error handling and parsing of checkpoints response
    
    * feat: enhance RewardsStreak and useRewardsHistory for improved loading state and checkpoint handling
    
    * feat: enhance RewardsHistory component to include service ID check and sort contract addresses by recent participation
    
    * refactor: update comments in RewardsHistory component for clarity on contract address sorting
    
    * refactor: rename service staking contract addresses for clarity and consistency in Gnosis chain
    
    * feat: update useRewardsHistory hook to filter checkpoints by supported staking contracts
    
    * feat: update RewardsStreak component to use Flex for layout and improve type safety in service functions

--- frontend/components/RewardsHistory/RewardsHistory.tsx
+++ frontend/components/RewardsHistory/RewardsHistory.tsx
@@ -29,13 +29,11 @@ import { UNICODE_SYMBOLS } from '@/constants/symbols';
 import { Pages } from '@/enums/PageState';
 import { StakingProgramId } from '@/enums/StakingProgram';
 import { usePageState } from '@/hooks/usePageState';
+import { useServices } from '@/hooks/useServices';
 import { balanceFormat } from '@/utils/numberFormatters';
 import { formatToMonthDay, formatToShortDateTime } from '@/utils/time';
 
-import {
-  TransformedCheckpoint,
-  useRewardsHistory,
-} from '../../hooks/useRewardsHistory';
+import { Checkpoint, useRewardsHistory } from '../../hooks/useRewardsHistory';
 import { EpochDetails } from './types';
 
 const { Text, Title } = Typography;
@@ -144,13 +142,15 @@ const EpochTime = ({ epoch }: { epoch: EpochDetails }) => {
   );
 };
 
+type ContractRewardsProps = {
+  stakingProgramId: StakingProgramId;
+  checkpoints: Checkpoint[];
+};
+
 const ContractRewards = ({
   stakingProgramId,
   checkpoints,
-}: {
-  stakingProgramId: StakingProgramId;
-  checkpoints: TransformedCheckpoint[];
-}) => (
+}: ContractRewardsProps) => (
   <Flex vertical>
     <ContractName>
       <Text strong>{STAKING_PROGRAM_META[stakingProgramId].name}</Text>
@@ -182,16 +182,36 @@ export const RewardsHistory = () => {
   const { contractCheckpoints, isError, isLoading, isFetching, refetch } =
     useRewardsHistory();
   const { goto } = usePageState();
+  const { serviceId } = useServices();
 
   const history = useMemo(() => {
-    if (isLoading || isFetching) return <Loading />;
+    if (isLoading || isFetching || !serviceId) return <Loading />;
     if (isError) return <ErrorLoadingHistory refetch={refetch} />;
     if (!contractCheckpoints) return <NoRewardsHistory />;
-    if (Object.keys(contractCheckpoints).length === 0)
+    if (Object.keys(contractCheckpoints).length === 0) {
       return <NoRewardsHistory />;
+    }
+
+    // find the recent contract address where the service has participated in
+    const recentContractAddress = Object.values(contractCheckpoints)
+      .flat()
+      .sort((a, b) => b.epochEndTimeStamp - a.epochEndTimeStamp)
+      .find((checkpoint) =>
+        checkpoint.serviceIds.includes(`${serviceId}`),
+      )?.contractAddress;
+
+    // most recent transaction staking contract at the top of the list
+    const latestContractAddresses = Object.keys(contractCheckpoints).sort(
+      (a, b) => {
+        if (a === recentContractAddress) return -1;
+        if (b === recentContractAddress) return 1;
+        return 0;
+      },
+    );
+
     return (
       <Flex vertical gap={16}>
-        {Object.keys(contractCheckpoints).map((contractAddress: string) => {
+        {latestContractAddresses.map((contractAddress: string) => {
           const checkpoints = contractCheckpoints[contractAddress];
           const [stakingProgramId] = Object.entries(
             SERVICE_STAKING_TOKEN_MECH_USAGE_CONTRACT_ADDRESSES[Chain.GNOSIS],
@@ -215,7 +235,7 @@ export const RewardsHistory = () => {
         })}
       </Flex>
     );
-  }, [isLoading, isFetching, isError, refetch, contractCheckpoints]);
+  }, [isLoading, isFetching, isError, serviceId, contractCheckpoints, refetch]);
 
   return (
     <ConfigProvider theme={yourWalletTheme}>
