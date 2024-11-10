cc643e5fe978dc0b6ffa7d7b8390a2dcecf90823
truemiller <mlxndrj@gmail.com>
2024-11-10 22:02:19 +0000

    feat: add mech and activity checker to OPTIMISM_STAKING_PROGRAMS configuration

--- frontend/config/stakingPrograms/optimism.ts
+++ frontend/config/stakingPrograms/optimism.ts
@@ -12,6 +12,8 @@ import { ChainId } from '@/enums/Chain';
 import { StakingProgramId } from '@/enums/StakingProgram';
 import { TokenSymbol } from '@/enums/Token';
 
+import { ACTIVITY_CHECKERS } from '../activityCheckers';
+import { MECHS, MechType } from '../mechs';
 import { StakingProgramMap } from '.';
 
 export const OPTIMISM_STAKING_PROGRAMS: StakingProgramMap = {
@@ -21,6 +23,8 @@ export const OPTIMISM_STAKING_PROGRAMS: StakingProgramMap = {
     stakingRequirements: {
       [TokenSymbol.OLAS]: 40,
     },
+    mech: MECHS[ChainId.Optimism][MechType.Agent].contract,
+    activityChecker: ACTIVITY_CHECKERS[ChainId.Optimism][MechType.Agent],
     chainId: ChainId.Optimism,
     contract: new MulticallContract(
       '0x88996bbdE7f982D93214881756840cE2c77C4992',
