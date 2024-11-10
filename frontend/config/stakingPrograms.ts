5028d8f20494c2a664aa1c5c39bb261575f6b0c5
truemiller <mlxndrj@gmail.com>
2024-11-10 14:39:22 +0000

    feat: staking program config with comments

--- frontend/config/stakingPrograms.ts
+++ frontend/config/stakingPrograms.ts
@@ -1,18 +1,40 @@
+/**
+ * Staking program configurations by chain
+ * @note Add new staking programs here
+ * @note Deprecated programs are hidden from the UI unless the user is already staked in the program
+ * @note Supported agents are the agents that can stake in the program
+ * @note Staking requirements are the minimum amount of tokens required to stake in staking program
+ * @note Update chain specfic configs only
+ * @note If creating a new chain, add a new key to the STAKING_PROGRAM_CONFIG object
+ */
+
 import { AgentType } from '@/enums/Agent';
 import { ChainId } from '@/enums/Chain';
 import { StakingProgramId } from '@/enums/StakingProgram';
 import { TokenSymbol } from '@/enums/Token';
 
-const GNOSIS_STAKING_PROGRAM_CONFIG: {
-  [stakingProgramId: string | StakingProgramId]: {
-    deprecated?: boolean; // hides program from UI unless user is already staked in this program
-    name: string;
-    supportedAgents: AgentType[];
-    stakingRequirements?: {
-      [tokenSymbol: string]: number;
-    };
+/**
+ * Single non-chain specific staking program configuration
+ */
+export type StakingProgramConfig = {
+  deprecated?: boolean; // hides program from UI unless user is already staked in this program
+  name: string;
+  supportedAgents: AgentType[];
+  stakingRequirements?: {
+    [tokenSymbol: string | TokenSymbol]: number;
   };
-} = {
+};
+
+/**
+ * Staking program configurations by staking program id
+ * @note Add new staking programs here
+ * @note Used to type chain specific staking program configs
+ */
+type StakingProgramConfigs = {
+  [stakingProgramId: string | StakingProgramId]: StakingProgramConfig;
+};
+
+const GNOSIS_STAKING_PROGRAM_CONFIG: StakingProgramConfigs = {
   [StakingProgramId.PearlAlpha]: {
     deprecated: true,
     name: 'Pearl Alpha',
@@ -76,7 +98,10 @@ const OPTIMISM_STAKING_PROGRAM_CONFIG = {
   },
 };
 
-export const STAKING_PROGRAM_CONFIG = {
+type StakingProgramConfigsByChain = {
+  [chainId: number | ChainId]: StakingProgramConfigs;
+};
+export const STAKING_PROGRAM_CONFIG: StakingProgramConfigsByChain = {
   [ChainId.Gnosis]: GNOSIS_STAKING_PROGRAM_CONFIG,
   [ChainId.Optimism]: OPTIMISM_STAKING_PROGRAM_CONFIG,
 };
