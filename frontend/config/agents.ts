97074b4af1b1712d8fabbf6ccf9c12fe54843505
truemiller <mlxndrj@gmail.com>
2024-11-10 21:59:22 +0000

    chore: import updated service names

--- frontend/config/agents.ts
+++ frontend/config/agents.ts
@@ -1,7 +1,7 @@
 import { AgentType } from '@/enums/Agent';
 import { ChainId } from '@/enums/Chain';
-import { OptimusServiceApi } from '@/service/agents/Optimus';
-import { PredictTraderServiceApi } from '@/service/agents/PredictTrader';
+import { OptimusService } from '@/service/agents/Optimus';
+import { PredictTraderService } from '@/service/agents/PredictTrader';
 
 export const AGENT_CONFIG = {
   [AgentType.PredictTrader]: {
@@ -9,13 +9,13 @@ export const AGENT_CONFIG = {
     homeChainId: ChainId.Gnosis,
     requiresAgentSafesOn: [ChainId.Gnosis],
     requiresMasterSafesOn: [ChainId.Gnosis],
-    serviceApi: PredictTraderServiceApi,
+    serviceApi: PredictTraderService,
   },
   [AgentType.Optimus]: {
     name: 'Optimus',
     homeChainId: ChainId.Optimism,
     requiresAgentSafesOn: [ChainId.Optimism, ChainId.Ethereum, ChainId.Base],
     requiresMasterSafesOn: [ChainId.Optimism, ChainId.Ethereum, ChainId.Base],
-    serviceApi: OptimusServiceApi,
+    serviceApi: OptimusService,
   },
 };
