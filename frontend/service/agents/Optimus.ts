7ea0812b0016d2974f399247eb81ef6c2c83b86c
truemiller <mlxndrj@gmail.com>
2024-11-10 22:50:39 +0000

    chore: remove optimus service methods

--- frontend/service/agents/Optimus.ts
+++ frontend/service/agents/Optimus.ts
@@ -1,21 +1,3 @@
-import { Contract as MulticallContract } from 'ethers-multicall';
-import { noop } from 'lodash';
-
 import { StakedAgentService } from './StakedAgentService';
 
-export class OptimusService extends StakedAgentService {
-  static activityCheckerContract: MulticallContract;
-  static stakingContracts: MulticallContract;
-  static serviceRegistryTokenUtilityContract: MulticallContract;
-
-  public static getStakingRewardsInfo = () => noop;
-
-  public static getAvailableRewardsForEpoch = () => noop;
-
-  public static getStakingContractInfo = () => noop;
-  private getStakingContractInfoByServiceIdStakingProgramId = () => noop;
-  private getStakingContractInfoByStakingProgramId = () => noop;
-
-  public static getServiceRegistryInfo = () => noop;
-  public static getCurrentStakingProgramByServiceId = () => noop;
-}
+export abstract class OptimusService extends StakedAgentService {}
