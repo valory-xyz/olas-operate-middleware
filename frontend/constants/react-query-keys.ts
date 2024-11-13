248a51abd77118e98ae9cbe244c73fd1fba67ab8
truemiller <mlxndrj@gmail.com>
2024-11-13 23:26:12 +0000

    feat: add wallets key to react-query keys for wallet provider

--- frontend/constants/react-query-keys.ts
+++ frontend/constants/react-query-keys.ts
@@ -1,5 +1,8 @@
 export const REACT_QUERY_KEYS = {
+  // service provider
   SERVICES_KEY: ['services'] as const,
   SERVICE_DEPLOYMENT_STATUS_KEY: (serviceConfigId: string) =>
     ['serviceStatus', serviceConfigId] as const,
+  // wallet provider
+  WALLETS_KEY: ['wallets'] as const,
 } as const;
