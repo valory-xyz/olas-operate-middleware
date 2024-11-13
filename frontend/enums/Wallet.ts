ea79538644cc809fd257f98865da5a2b7c0d6f60
truemiller <mlxndrj@gmail.com>
2024-11-13 22:27:18 +0000

    feat: add EoaWallet and SafeWallet types to Wallet enums

--- frontend/enums/Wallet.ts
+++ frontend/enums/Wallet.ts
@@ -1,3 +1,7 @@
+import { Address } from '@/types/Address';
+
+import { ChainId } from './Chain';
+
 export enum WalletType {
   Safe = 'multisig',
   EOA = 'eoa',
@@ -7,3 +11,16 @@ export enum WalletOwner {
   Master = 'master', // user
   Agent = 'agent',
 }
+
+export type EoaWallet = {
+  address: Address;
+  type: WalletType.EOA;
+  owner: WalletOwner;
+};
+
+export type SafeWallet = {
+  address: Address;
+  type: WalletType.Safe;
+  owner: WalletOwner;
+  chainId: ChainId;
+};
