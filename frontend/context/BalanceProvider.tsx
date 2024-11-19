30d83e87905c94db9c96de4486528c5507d7a0c5
truemiller <mlxndrj@gmail.com>
2024-11-19 23:15:25 +0000

    feat: wallet balances, and staked balances, cross-chain

--- frontend/context/BalanceProvider.tsx
+++ frontend/context/BalanceProvider.tsx
@@ -1,9 +1,5 @@
-import { message } from 'antd';
 import { BigNumberish } from 'ethers';
-import { isAddress } from 'ethers/lib/utils';
 import { Contract as MulticallContract } from 'ethers-multicall';
-import { isNil } from 'lodash';
-import { ValueOf } from 'next/dist/shared/lib/constants';
 import {
   createContext,
   Dispatch,
@@ -11,23 +7,24 @@ import {
   SetStateAction,
   useCallback,
   useContext,
+  useEffect,
   useState,
 } from 'react';
-import { useInterval } from 'usehooks-ts';
 
 import { ERC20_BALANCE_OF_STRING_FRAGMENT } from '@/abis/erc20';
-import { CHAIN_CONFIG } from '@/config/chains';
+import { MiddlewareServiceResponse } from '@/client';
 import { TOKEN_CONFIG, TokenType } from '@/config/tokens';
-import { FIVE_SECONDS_INTERVAL } from '@/constants/intervals';
 import { PROVIDERS } from '@/constants/providers';
 import { ChainId } from '@/enums/Chain';
 import { ServiceRegistryL2ServiceState } from '@/enums/ServiceRegistryL2ServiceState';
 import { TokenSymbol } from '@/enums/Token';
 import { Wallets, WalletType } from '@/enums/Wallet';
 import { useServices } from '@/hooks/useServices';
-import { StakedAgentService } from '@/service/agents/StakedAgentService';
+import {
+  GetServiceRegistryInfoResponse,
+  StakedAgentService,
+} from '@/service/agents/StakedAgentService';
 import { Address } from '@/types/Address';
-import { WalletAddressNumberRecord } from '@/types/Records';
 
 import { OnlineStatusContext } from './OnlineStatusProvider';
 import { WalletContext } from './WalletProvider';
@@ -35,52 +32,53 @@ import { WalletContext } from './WalletProvider';
 export const BalanceContext = createContext<{
   isLoaded: boolean;
   setIsLoaded: Dispatch<SetStateAction<boolean>>;
-  isBalanceLoaded: boolean;
-  olasBondBalance?: number;
-  olasDepositBalance?: number;
-  masterEoaBalance?: ValueOf<WalletAddressNumberRecord>;
-  masterSafeBalance?: ValueOf<WalletAddressNumberRecord>;
-  totalEthBalance?: number;
   totalOlasBalance?: number;
   isLowBalance: boolean;
-  wallets?: Wallets[];
-  walletBalances: WalletAddressNumberRecord;
-  agentSafeBalance?: ValueOf<WalletAddressNumberRecord>;
-  agentEoaBalance?: ValueOf<WalletAddressNumberRecord>;
   updateBalances: () => Promise<void>;
   setIsPaused: Dispatch<SetStateAction<boolean>>;
-  totalOlasStakedBalance?: number;
+  walletBalances: BalanceResult[];
+  stakedBalances: CrossChainStakedBalances;
 }>({
   isLoaded: false,
   setIsLoaded: () => {},
-  isBalanceLoaded: false,
-  olasBondBalance: undefined,
-  olasDepositBalance: undefined,
-  totalEthBalance: undefined,
   totalOlasBalance: undefined,
   isLowBalance: false,
-  wallets: undefined,
-  walletBalances: {},
-  agentSafeBalance: undefined,
-  agentEoaBalance: undefined,
   updateBalances: async () => {},
   setIsPaused: () => {},
-  totalOlasStakedBalance: undefined,
+  walletBalances: [],
+  stakedBalances: {},
 });
 
 export const BalanceProvider = ({ children }: PropsWithChildren) => {
   const { isOnline } = useContext(OnlineStatusContext);
   const { wallets } = useContext(WalletContext);
-  const { services, serviceAddresses } = useServices();
+  const { services } = useServices();
   // const { optimisticRewardsEarnedForEpoch, accruedServiceStakingRewards } =
   //   useContext(RewardContext);
 
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isPaused, setIsPaused] = useState<boolean>(false);
 
-  const [isBalanceLoaded, setIsBalanceLoaded] = useState<boolean>(false);
-
   const [walletBalances, setWalletBalances] = useState<BalanceResult[]>([]);
+  const [stakedBalances, setStakedBalances] =
+    useState<CrossChainStakedBalances>();
+
+  const updateBalances = useCallback(async () => {
+    if (wallets && services) {
+      getCrossChainWalletBalances(wallets).then((balances) => {
+        setWalletBalances(balances);
+        setIsLoaded(true);
+      });
+
+      getCrossChainStakedBalances(services).then((balances) => {
+        setStakedBalances(balances);
+      });
+    }
+  }, [services, wallets]);
+
+  useEffect(() => {
+    if (!isLoaded) updateBalances();
+  }, [isLoaded, updateBalances]);
 
   // // TODO: refactor to parse `walletbalances`
 
@@ -110,102 +108,40 @@ export const BalanceProvider = ({ children }: PropsWithChildren) => {
   //   return total;
   // }, [accruedServiceStakingRewards, isLoaded, optimisticRewardsEarnedForEpoch]);
 
-  const totalOlasStakedBalance: number | undefined = useMemo(() => {
-    if (!isLoaded) return;
-    return (olasBondBalance ?? 0) + (olasDepositBalance ?? 0);
-  }, [isLoaded]);
+  // const totalOlasStakedBalance: number | undefined = useMemo(() => {
+  //   if (!isLoaded) return;
+  //   return (olasBondBalance ?? 0) + (olasDepositBalance ?? 0);
+  // }, [isLoaded]);
 
-  const updateBalances = useCallback(async (): Promise<void> => {
-    if (!wallets) return;
+  // // TODO: include in walletBalances
+  // // const isLowBalance = useMemo(() => {
+  // //   if (!masterSafeBalance || !agentSafeBalance) return false;
+  // //   if (
+  // //     masterSafeBalance.ETH < LOW_MASTER_SAFE_BALANCE &&
+  // //     // Need to check agentSafe balance as well, because it's auto-funded from safeBalance
+  // //     agentSafeBalance.ETH < LOW_AGENT_SAFE_BALANCE
+  // //   )
+  // //     return true;
+  // //   return false;
+  // // }, [masterSafeBalance, agentSafeBalance]);
 
-    const walletBalances = await getCrossChainWalletBalances(wallets);
-    setWalletBalances(walletBalances);
-
-    try {
-      // TODO: refactor to use ChainId enum, service from useService(),
-      const serviceId =
-        services?.[0]?.chain_configs[CHAIN_CONFIG.OPTIMISM.chainId].chain_data
-          .token;
-
-      if (
-        !isNil(masterSafeAddress) &&
-        isAddress(masterSafeAddress) &&
-        serviceId > 0
-      ) {
-        const { depositValue, bondValue, serviceState } =
-          await StakedAgentService.getServiceRegistryInfo(
-            masterSafeAddress,
-            serviceId,
-            ChainId.Gnosis, // TODO: refactor to get chain id from service
-          );
-
-        switch (serviceState) {
-          case ServiceRegistryL2ServiceState.NonExistent:
-            setOlasBondBalance(0);
-            setOlasDepositBalance(0);
-            break;
-          case ServiceRegistryL2ServiceState.PreRegistration:
-            setOlasBondBalance(0);
-            setOlasDepositBalance(0);
-            break;
-          case ServiceRegistryL2ServiceState.ActiveRegistration:
-            setOlasBondBalance(0);
-            setOlasDepositBalance(depositValue);
-            break;
-          case ServiceRegistryL2ServiceState.FinishedRegistration:
-            setOlasBondBalance(bondValue);
-            setOlasDepositBalance(depositValue);
-            break;
-          case ServiceRegistryL2ServiceState.Deployed:
-            setOlasBondBalance(bondValue);
-            setOlasDepositBalance(depositValue);
-            break;
-          case ServiceRegistryL2ServiceState.TerminatedBonded:
-            setOlasBondBalance(bondValue);
-            setOlasDepositBalance(0);
-            break;
-        }
-      }
-
-      // update balance loaded state
-      setIsLoaded(true);
-      setIsBalanceLoaded(true);
-    } catch (error) {
-      console.error(error);
-      message.error('Unable to retrieve wallet balances');
-      setIsBalanceLoaded(true);
-    }
-  }, [services, wallets]);
-
-  // TODO: include in walletBalances
-  // const isLowBalance = useMemo(() => {
-  //   if (!masterSafeBalance || !agentSafeBalance) return false;
-  //   if (
-  //     masterSafeBalance.ETH < LOW_MASTER_SAFE_BALANCE &&
-  //     // Need to check agentSafe balance as well, because it's auto-funded from safeBalance
-  //     agentSafeBalance.ETH < LOW_AGENT_SAFE_BALANCE
-  //   )
-  //     return true;
-  //   return false;
-  // }, [masterSafeBalance, agentSafeBalance]);
-
-  useInterval(
-    () => {
-      updateBalances();
-    },
-    isPaused || !isOnline ? null : FIVE_SECONDS_INTERVAL,
-  );
+  // useInterval(
+  //   () => {
+  //     updateBalances();
+  //   },
+  //   isPaused || !isOnline ? null : FIVE_SECONDS_INTERVAL,
+  // );
 
   return (
     <BalanceContext.Provider
       value={{
         isLoaded,
         setIsLoaded,
-        isBalanceLoaded,
-        wallets,
         walletBalances,
+        stakedBalances: stakedBalances || {},
         updateBalances,
         setIsPaused,
+        isLowBalance: false,
       }}
     >
       {children}
@@ -330,61 +266,125 @@ const getCrossChainWalletBalances = async (
 
 // TODO: implement staked balances cross-chain for all master safes
 
+type CrossChainStakedBalances = {
+  [serviceId: string]: {
+    [chainId: number]: {
+      olasBondBalance: number;
+      olasDepositBalance: number;
+    };
+  };
+};
+
 const getCrossChainStakedBalances = async (
   services: MiddlewareServiceResponse[],
-  wallets: MasterSafe[],
-): Promise<
-  {
-    address: `0x${string}`;
-    olasBondBalance?: number;
-    olasDepositBalance?: number;
-  }[]
-> => {
-  const serviceId =
-    services?.[0]?.chain_configs[CHAIN_CONFIG.OPTIMISM.chainId].chain_data
-      .token;
+): Promise<CrossChainStakedBalances> => {
+  const result: CrossChainStakedBalances = {};
 
-  if (
-    !isNil(masterSafeAddress) &&
-    isAddress(masterSafeAddress) &&
-    serviceId > 0
-  ) {
-    const { depositValue, bondValue, serviceState } =
-      await StakedAgentService.getServiceRegistryInfo(
-        masterSafeAddress,
-        serviceId,
-        ChainId.Gnosis, // TODO: refactor to get chain id from service
+  // for each service, create a nest objects for chain staked balances
+  const registryInfoCalls: Promise<GetServiceRegistryInfoResponse>[] = [];
+  services.forEach((service) => {
+    const homeChainId = service.home_chain_id;
+    const homeChainConfig = service.chain_configs[homeChainId];
+    const { multisig, token } = homeChainConfig.chain_data;
+
+    if (!multisig || !token) {
+      registryInfoCalls.push(
+        Promise.resolve({
+          depositValue: 0,
+          bondValue: 0,
+          serviceState: ServiceRegistryL2ServiceState.NonExistent,
+        }),
       );
-
-    switch (serviceState) {
-      case ServiceRegistryL2ServiceState.NonExistent:
-        setOlasBondBalance(0);
-        setOlasDepositBalance(0);
-        break;
-      case ServiceRegistryL2ServiceState.PreRegistration:
-        setOlasBondBalance(0);
-        setOlasDepositBalance(0);
-        break;
-      case ServiceRegistryL2ServiceState.ActiveRegistration:
-        setOlasBondBalance(0);
-        setOlasDepositBalance(depositValue);
-        break;
-      case ServiceRegistryL2ServiceState.FinishedRegistration:
-        setOlasBondBalance(bondValue);
-        setOlasDepositBalance(depositValue);
-        break;
-      case ServiceRegistryL2ServiceState.Deployed:
-        setOlasBondBalance(bondValue);
-        setOlasDepositBalance(depositValue);
-        break;
-      case ServiceRegistryL2ServiceState.TerminatedBonded:
-        setOlasBondBalance(bondValue);
-        setOlasDepositBalance(0);
-        break;
+      return;
     }
-  }
 
-  // update balance loaded state
-  setIsLoaded(true);
-  setIsBalanceLoaded(true);
+    registryInfoCalls.push(
+      StakedAgentService.getServiceRegistryInfo(multisig, token, homeChainId),
+    );
+  });
+
+  // TODO: chunk and batch multicalls by chain,
+  // currently Promise is returned by `StakedAgentService.getServiceRegistryInfo`
+  // will not scale well with large number of services
+  const registryInfos = await Promise.allSettled(registryInfoCalls);
+
+  registryInfos.forEach((res, index) => {
+    if (res.status === 'fulfilled') {
+      const { depositValue, bondValue, serviceState } =
+        res.value as GetServiceRegistryInfoResponse;
+      const serviceId = services[index].service_config_id;
+      const homeChainId = services[index].home_chain_id;
+
+      if (!result[serviceId]) result[serviceId] = {};
+      if (!result[serviceId][homeChainId])
+        result[serviceId][homeChainId] = {
+          olasBondBalance: 0,
+          olasDepositBalance: 0,
+        };
+
+      // service state is used to determine the correct bond and deposit balances
+      // stops funds from being displayed incorrectly during service state transitions
+      const serviceStateBondDepositBalances = correctBondDepositByServiceState({
+        olasBondBalance: bondValue,
+        olasDepositBalance: depositValue,
+        serviceState,
+      });
+
+      result[serviceId][homeChainId] = serviceStateBondDepositBalances;
+    }
+  });
+
+  return result;
+};
+
+/**
+ * Corrects the bond and deposit balances based on the service state
+ * @note the service state is used to determine the correct bond and deposit balances
+ */
+const correctBondDepositByServiceState = ({
+  olasBondBalance,
+  olasDepositBalance,
+  serviceState,
+}: {
+  olasBondBalance: number;
+  olasDepositBalance: number;
+  serviceState: ServiceRegistryL2ServiceState;
+}): {
+  olasBondBalance: number;
+  olasDepositBalance: number;
+} => {
+  switch (serviceState) {
+    case ServiceRegistryL2ServiceState.NonExistent:
+      return {
+        olasBondBalance: 0,
+        olasDepositBalance: 0,
+      };
+    case ServiceRegistryL2ServiceState.PreRegistration:
+      return {
+        olasBondBalance: 0,
+        olasDepositBalance: 0,
+      };
+    case ServiceRegistryL2ServiceState.ActiveRegistration:
+      return {
+        olasBondBalance: 0,
+        olasDepositBalance,
+      };
+    case ServiceRegistryL2ServiceState.FinishedRegistration:
+      return {
+        olasBondBalance,
+        olasDepositBalance,
+      };
+    case ServiceRegistryL2ServiceState.Deployed:
+      return {
+        olasBondBalance,
+        olasDepositBalance,
+      };
+    case ServiceRegistryL2ServiceState.TerminatedBonded:
+      return {
+        olasBondBalance,
+        olasDepositBalance: 0,
+      };
+    default:
+      throw new Error('Invalid service state');
+  }
 };
