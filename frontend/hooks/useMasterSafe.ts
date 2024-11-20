82f8963d815ec778359a398d1bf275959eeeb5da
truemiller <mlxndrj@gmail.com>
2024-11-20 16:34:40 +0000

    refactor: update useMasterSafe hook to use react-query for fetching owners

--- frontend/hooks/useMasterSafe.ts
+++ frontend/hooks/useMasterSafe.ts
@@ -1,7 +1,22 @@
-import { useContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Contract } from 'ethers';
 
-import { MasterSafeContext } from '@/context/MasterSafeProvider';
+import { GNOSIS_SAFE_ABI } from '@/abis/gnosisSafe';
+import { PROVIDERS } from '@/constants/providers';
+import { REACT_QUERY_KEYS } from '@/constants/react-query-keys';
+import { MasterSafe } from '@/enums/Wallet';
+import { Address } from '@/types/Address';
 
-export const useMasterSafe = () => {
-  return useContext(MasterSafeContext);
+export const useMasterSafe = (masterSafe: MasterSafe) => {
+  return useQuery<Address[]>({
+    queryKey: REACT_QUERY_KEYS.MULTISIG_GET_OWNERS_KEY(masterSafe),
+    queryFn: async () => {
+      const contract = new Contract(
+        masterSafe.address,
+        GNOSIS_SAFE_ABI,
+        PROVIDERS[masterSafe.chainId].provider,
+      );
+      return contract.functions.getOwners() as Promise<Address[]>;
+    },
+  });
 };
