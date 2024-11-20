f986a5a711f4d12d10594279016018409e83c206
truemiller <mlxndrj@gmail.com>
2024-11-20 16:36:37 +0000

    refactor: replace useMasterSafe with useMultisig for more generic approach

--- frontend/hooks/useMultisig.ts
+++ frontend/hooks/useMultisig.ts
@@ -4,17 +4,23 @@ import { Contract } from 'ethers';
 import { GNOSIS_SAFE_ABI } from '@/abis/gnosisSafe';
 import { PROVIDERS } from '@/constants/providers';
 import { REACT_QUERY_KEYS } from '@/constants/react-query-keys';
-import { MasterSafe } from '@/enums/Wallet';
+import { Safe } from '@/enums/Wallet';
 import { Address } from '@/types/Address';
 
-export const useMasterSafe = (masterSafe: MasterSafe) => {
+/**
+ * Hook to fetch multisig owners
+ * @param safe
+ * @returns multisig owners
+ * @note extend with further multisig functions as needed
+ */
+export const useMultisig = (safe: Safe) => {
   return useQuery<Address[]>({
-    queryKey: REACT_QUERY_KEYS.MULTISIG_GET_OWNERS_KEY(masterSafe),
+    queryKey: REACT_QUERY_KEYS.MULTISIG_GET_OWNERS_KEY(safe),
     queryFn: async () => {
       const contract = new Contract(
-        masterSafe.address,
+        safe.address,
         GNOSIS_SAFE_ABI,
-        PROVIDERS[masterSafe.chainId].provider,
+        PROVIDERS[safe.chainId].provider,
       );
       return contract.functions.getOwners() as Promise<Address[]>;
     },
