e6269acade041e2baf35f25cf5d3ca00a1d1b133
truemiller <mlxndrj@gmail.com>
2024-11-21 18:00:38 +0000

    refactor: update YourWalletPage to use updated types, contexts, hooks

--- frontend/components/YourWalletPage/index.tsx
+++ frontend/components/YourWalletPage/index.tsx
@@ -1,16 +1,28 @@
 import { CloseOutlined } from '@ant-design/icons';
-import { Button, ConfigProvider, Flex, ThemeConfig, Typography } from 'antd';
+import {
+  Button,
+  ConfigProvider,
+  Flex,
+  Skeleton,
+  ThemeConfig,
+  Typography,
+} from 'antd';
+import { isEmpty, isNil } from 'lodash';
 import { useMemo } from 'react';
 
 import { AddressLink } from '@/components/AddressLink';
 import { CardTitle } from '@/components/Card/CardTitle';
 import { InfoBreakdownList } from '@/components/InfoBreakdown';
 import { CardFlex } from '@/components/styled/CardFlex';
+import { getNativeTokenSymbol } from '@/config/tokens';
+import { ChainId } from '@/enums/Chain';
 import { Pages } from '@/enums/Pages';
+import { TokenSymbol } from '@/enums/Token';
 import { useBalanceContext } from '@/hooks/useBalanceContext';
 import { usePageState } from '@/hooks/usePageState';
-import { useServices } from '@/hooks/useServices';
-import { useWallet } from '@/hooks/useWallet';
+import { useMasterWalletContext } from '@/hooks/useWallet';
+import { type Address } from '@/types/Address';
+import { Optional } from '@/types/Util';
 import { balanceFormat } from '@/utils/numberFormatters';
 
 import { Container, infoBreakdownParentStyle } from './styles';
@@ -25,10 +37,15 @@ const yourWalletTheme: ThemeConfig = {
   },
 };
 
-const YourWalletTitle = () => <CardTitle title="Your wallet" />;
+const YourWalletTitle = () => <CardTitle title="Your wallets" />;
 
 const Address = () => {
-  const { masterSafeAddress } = useWallet();
+  const { masterSafes } = useMasterWalletContext();
+
+  if (!masterSafes) return <Skeleton />;
+  if (isEmpty(masterSafes)) return null;
+
+  const masterSafeAddress = masterSafes[0].address; // TODO: handle multiple safes in future
 
   return (
     <Flex vertical gap={8}>
@@ -48,29 +65,37 @@ const Address = () => {
 };
 
 const OlasBalance = () => {
-  const { masterSafeBalance: safeBalance, totalOlasStakedBalance } =
-    useBalanceContext();
+  const { masterSafes } = useMasterWalletContext();
+  const { walletBalances, totalStakedOlasBalance } = useBalanceContext();
+
+  const masterSafeOlasBalance: number = walletBalances
+    ?.filter((balance) => balance.symbol === TokenSymbol.OLAS)
+    .reduce((acc, balance) => acc + balance.balance, 0);
+
   const olasBalances = useMemo(() => {
     return [
       {
         title: 'Available',
-        value: balanceFormat(safeBalance?.OLAS ?? 0, 2),
+        value: balanceFormat(masterSafeOlasBalance ?? 0, 2),
       },
       {
         title: 'Staked',
-        value: balanceFormat(totalOlasStakedBalance ?? 0, 2),
+        value: balanceFormat(totalStakedOlasBalance ?? 0, 2),
       },
     ];
-  }, [safeBalance?.OLAS, totalOlasStakedBalance]);
+  }, [masterSafeOlasBalance, totalStakedOlasBalance]);
+
+  if (!masterSafes) return <Skeleton />;
+  if (isEmpty(masterSafes)) return null;
 
   return (
     <Flex vertical gap={8}>
-      <Text strong>OLAS</Text>
+      <Text strong>{TokenSymbol.OLAS}</Text>
       <InfoBreakdownList
         list={olasBalances.map((item) => ({
           left: item.title,
           leftClassName: 'text-light',
-          right: `${item.value} OLAS`,
+          right: `${item.value} ${TokenSymbol.OLAS}`,
         }))}
         parentStyle={infoBreakdownParentStyle}
       />
@@ -78,17 +103,38 @@ const OlasBalance = () => {
   );
 };
 
-const XdaiBalance = () => {
-  const { masterSafeBalance: safeBalance } = useBalanceContext();
+const MasterSafeNativeBalance = () => {
+  const { masterSafes, masterEoa } = useMasterWalletContext();
+  const { walletBalances } = useBalanceContext();
+
+  const masterSafeNativeBalance: Optional<number> = useMemo(() => {
+    if (isNil(masterSafes)) return;
+    if (isNil(walletBalances)) return;
+
+    if (isEmpty(masterSafes)) return 0;
+    if (isEmpty(walletBalances)) return 0;
+
+    const masterSafe = masterSafes[0]; // TODO: handle multiple safes in future
+
+    return walletBalances
+      .filter(
+        ({ walletAddress }) =>
+          walletAddress === masterSafe.address || // TODO: handle multiple safes in future
+          walletAddress === masterEoa?.address,
+      )
+      .reduce((acc, balance) => acc + balance.balance, 0);
+  }, [masterEoa?.address, masterSafes, walletBalances]);
+
+  const nativeTokenSymbol = getNativeTokenSymbol(ChainId.Gnosis);
 
   return (
     <Flex vertical gap={8}>
       <InfoBreakdownList
         list={[
           {
-            left: <Text strong>XDAI</Text>,
+            left: <Text strong>{getNativeTokenSymbol(ChainId.Gnosis)}</Text>,
             leftClassName: 'text-light',
-            right: `${balanceFormat(safeBalance?.ETH, 2)} XDAI`,
+            right: `${balanceFormat(masterSafeNativeBalance, 2)} ${nativeTokenSymbol}`,
           },
         ]}
         parentStyle={infoBreakdownParentStyle}
@@ -97,9 +143,27 @@ const XdaiBalance = () => {
   );
 };
 
-const Signer = () => {
-  const { masterEoaAddress } = useWallet();
-  const { masterEoaBalance: eoaBalance } = useBalanceContext();
+const MasterEoaSignerNativeBalance = () => {
+  const { masterEoa } = useMasterWalletContext();
+  const { walletBalances } = useBalanceContext();
+
+  const masterEoaBalance = useMemo(() => {
+    if (isNil(masterEoa)) return;
+    if (isNil(walletBalances)) return;
+
+    return walletBalances
+      .filter(
+        (
+          { walletAddress, isNative }, // TODO: support chainId grouping, for multi-agent
+        ) => walletAddress === masterEoa.address && isNative,
+      )
+      .reduce((acc, balance) => acc + balance.balance, 0);
+  }, [masterEoa, walletBalances]);
+
+  const nativeTokenSymbol = useMemo(
+    () => getNativeTokenSymbol(ChainId.Gnosis),
+    [],
+  ); // TODO: support multi chain
 
   return (
     <Flex vertical gap={8}>
@@ -109,11 +173,11 @@ const Signer = () => {
             left: (
               <SignerTitle
                 signerText="Your wallet signer address:"
-                signerAddress={masterEoaAddress}
+                signerAddress={masterEoa?.address}
               />
             ),
             leftClassName: 'text-light',
-            right: `${balanceFormat(eoaBalance?.ETH, 2)} XDAI`,
+            right: `${balanceFormat(masterEoaBalance, 2)} ${nativeTokenSymbol}`,
           },
         ]}
         parentStyle={infoBreakdownParentStyle}
@@ -124,7 +188,6 @@ const Signer = () => {
 
 export const YourWalletPage = () => {
   const { goto } = usePageState();
-  const { service } = useServices();
 
   return (
     <ConfigProvider theme={yourWalletTheme}>
@@ -142,9 +205,9 @@ export const YourWalletPage = () => {
         <Container style={{ margin: 8 }}>
           <Address />
           <OlasBalance />
-          <XdaiBalance />
-          <Signer />
-          {service && <YourAgentWallet />}
+          <MasterSafeNativeBalance />
+          <MasterEoaSignerNativeBalance />
+          <YourAgentWallet />
         </Container>
       </CardFlex>
     </ConfigProvider>
