492bec66231119bc7d3db7076e2977ac3b46b3c3
truemiller <mlxndrj@gmail.com>
2024-12-02 11:05:55 +0000

    fix: replace 0xstring

--- frontend/types/Address.ts
+++ frontend/types/Address.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export type Address = string;
+export type Address = `0x${string}`;
 
 export const AddressSchema = z.string().refine((value) => {
   return /^0x[0-9a-fA-F]{40}$/.test(value);
