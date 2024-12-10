9de0de02410810313c5aa85b4c74be49c128ac15
Mohan <mohandast52@gmail.com>
2024-12-10 17:01:56 +0530

    feat: Agent animation when running (#562)
    * fix: add lottie-react dependency
    
    * feat: add Lottie animation to SetupWelcome component
    
    * refactor: remove unused Lottie animation from SetupWelcome and integrate into DeployedAgentHead
    
    * refactor: adjust dimensions and positioning of AnimationContainer in DeployedAgentHead
    
    * refactor: adjust top positioning of AnimationContainer in AgentHead component
    
    * fix: add missing newline at end of package.json

--- frontend/components/MainPage/header/AgentHead.tsx
+++ frontend/components/MainPage/header/AgentHead.tsx
@@ -1,5 +1,7 @@
 import { Badge } from 'antd';
+import { useLottie } from 'lottie-react';
 import Image from 'next/image';
+import styled from 'styled-components';
 
 import { MiddlewareDeploymentStatus } from '@/client';
 import { useReward } from '@/hooks/useReward';
@@ -7,17 +9,33 @@ import { useServices } from '@/hooks/useServices';
 
 const badgeOffset: [number, number] = [-5, 32.5];
 
+const AnimationContainer = styled.div`
+  position: relative;
+  top: -4px;
+  width: 42px;
+  height: 42px;
+  padding: 2px 0;
+  > div {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
 const TransitionalAgentHead = () => (
   <Badge status="processing" color="orange" dot offset={badgeOffset}>
     <Image src="/happy-robot.svg" alt="Happy Robot" width={40} height={40} />
   </Badge>
 );
 
-const DeployedAgentHead = () => (
-  <Badge status="processing" color="green" dot offset={badgeOffset}>
-    <Image src="/happy-robot.svg" alt="Happy Robot" width={40} height={40} />
-  </Badge>
-);
+const DeployedAgentHead = () => {
+  const { View } = useLottie({
+    animationData: require('../../ui/animations/robot-running.json'),
+    loop: true,
+    autoplay: true,
+  });
+
+  return <AnimationContainer>{View}</AnimationContainer>;
+};
 
 const StoppedAgentHead = () => (
   <Badge dot color="red" offset={badgeOffset}>
