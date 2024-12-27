7e3f8544bd1479269fa835e2bd31d40a4fc27bba
Atatakai <densatsu@mail.ru>
2024-12-27 17:27:46 +0400

    chore: fix review comment

--- frontend/components/MainPage/header/AgentButton/AgentNotRunningButton.tsx
+++ frontend/components/MainPage/header/AgentButton/AgentNotRunningButton.tsx
@@ -7,6 +7,7 @@ import { CHAIN_CONFIG } from '@/config/chains';
 import { MechType } from '@/config/mechs';
 import { STAKING_PROGRAMS } from '@/config/stakingPrograms';
 import { SERVICE_TEMPLATES } from '@/constants/serviceTemplates';
+import { AgentType } from '@/enums/Agent';
 import { Pages } from '@/enums/Pages';
 import { TokenSymbol } from '@/enums/Token';
 import {
@@ -257,7 +258,13 @@ const useServiceDeployment = () => {
     }
 
     // Temporary: update the service if it has the default description
-    if (service && service.description === 'Memeooorr @twitter_handle') {
+    if (
+      service &&
+      service.description ===
+        SERVICE_TEMPLATES.find(
+          (template) => template.agentType === AgentType.Memeooorr,
+        )?.description
+    ) {
       const xUsername = service.env_variables?.TWIKIT_USERNAME?.value;
       if (xUsername) {
         await ServicesService.updateService({
