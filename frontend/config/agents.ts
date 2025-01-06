d8bdc30f215e5ee908a5e1acea7c57b8cf3eee71
Atatakai <densatsu@mail.ru>
2025-01-06 15:19:00 +0400

    release: 0.2.0-rc71

--- frontend/config/agents.ts
+++ frontend/config/agents.ts
@@ -114,7 +114,7 @@ export const AGENT_CONFIG: {
     displayName: 'Agents.fun agent',
     description:
       'Autonomously post to Twitter, create and trade memecoins, and interact with other agents.',
-    isAgentEnabled: true,
+    isAgentEnabled: false,
   },
   [AgentType.Modius]: {
     name: 'Modius agent',
@@ -159,6 +159,6 @@ export const AGENT_CONFIG: {
     displayName: 'Modius agent',
     description:
       'Invests crypto assets on your behalf and grows your portfolio.',
-    isAgentEnabled: false,
+    isAgentEnabled: true,
   },
 };
--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -291,7 +291,7 @@ export const SERVICE_TEMPLATES: ServiceTemplate[] = [
         threshold: 1,
         use_staking: true,
         cost_of_bond: +parseEther(20),
-        monthly_gas_estimate: +parseEther(0.006),
+        monthly_gas_estimate: +parseEther(0.011), // TODO: should be 0.0055, temp fix to avoid low balance alerts until the refund is fixed in the middleware
         fund_requirements: {
           [ethers.constants.AddressZero]: {
             // zero address means native currency
