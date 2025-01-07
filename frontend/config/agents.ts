0378658c4ba39610a57ee82a59d20e86b1e20efa
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2025-01-07 17:39:40 +0100

    chore: disable memeooorr agent

--- frontend/config/agents.ts
+++ frontend/config/agents.ts
@@ -93,7 +93,7 @@ export const AGENT_CONFIG: {
     displayName: 'Agents.fun agent',
     description:
       'Autonomously post to Twitter, create and trade memecoins, and interact with other agents.',
-    isAgentEnabled: true,
+    isAgentEnabled: false,
   },
   [AgentType.Modius]: {
     name: 'Modius agent',
