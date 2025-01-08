bf2fa769354a8516d0645882cac26faba5fc992f
mohandast52 <mohandast52@gmail.com>
2025-01-09 02:15:53 +0530

    refactor: remove unused fund requirements and operating thresholds from agent configurations

--- frontend/config/agents.ts
+++ frontend/config/agents.ts
@@ -1,31 +1,16 @@
-import { ethers } from 'ethers';
 import { formatUnits } from 'ethers/lib/utils';
 
 import { MiddlewareChain } from '@/client';
-import {
-  MEMEOOORR_BASE_TEMPLATE,
-  MODIUS_SERVICE_TEMPLATE,
-  PREDICT_SERVICE_TEMPLATE,
-} from '@/constants/serviceTemplates';
+import { MODIUS_SERVICE_TEMPLATE } from '@/constants/serviceTemplates';
 import { AgentType } from '@/enums/Agent';
 import { EvmChainId } from '@/enums/Chain';
 import { TokenSymbol } from '@/enums/Token';
-import { WalletOwnerType, WalletType } from '@/enums/Wallet';
 import { MemeooorBaseService } from '@/service/agents/Memeooor';
 import { ModiusService } from '@/service/agents/Modius';
 import { PredictTraderService } from '@/service/agents/PredictTrader';
 import { AgentConfig } from '@/types/Agent';
-import { formatEther } from '@/utils/numberFormatters';
 
 import { MODE_TOKEN_CONFIG } from './tokens';
-
-const traderFundRequirements =
-  PREDICT_SERVICE_TEMPLATE.configurations[MiddlewareChain.GNOSIS]
-    .fund_requirements[ethers.constants.AddressZero];
-
-const memeooorrRequirements =
-  MEMEOOORR_BASE_TEMPLATE.configurations[MiddlewareChain.BASE]
-    .fund_requirements[ethers.constants.AddressZero];
 
 const modiusFundRequirements =
   MODIUS_SERVICE_TEMPLATE.configurations[MiddlewareChain.MODE]
@@ -39,25 +24,6 @@ export const AGENT_CONFIG: {
     evmHomeChainId: EvmChainId.Gnosis,
     middlewareHomeChainId: MiddlewareChain.GNOSIS,
     requiresAgentSafesOn: [EvmChainId.Gnosis],
-    operatingThresholds: {
-      [WalletOwnerType.Master]: {
-        [WalletType.Safe]: {
-          [TokenSymbol.XDAI]: Number(
-            formatEther(`${traderFundRequirements.safe}`),
-          ),
-        },
-        [WalletType.EOA]: {
-          [TokenSymbol.XDAI]: 0.1, // TODO: should come from the template
-        },
-      },
-      [WalletOwnerType.Agent]: {
-        [WalletType.Safe]: {
-          [TokenSymbol.XDAI]: Number(
-            formatEther(`${traderFundRequirements.agent}`),
-          ),
-        },
-      },
-    },
     requiresMasterSafesOn: [EvmChainId.Gnosis],
     serviceApi: PredictTraderService,
     displayName: 'Prediction agent',
@@ -69,25 +35,6 @@ export const AGENT_CONFIG: {
     evmHomeChainId: EvmChainId.Base,
     middlewareHomeChainId: MiddlewareChain.BASE,
     requiresAgentSafesOn: [EvmChainId.Base],
-    operatingThresholds: {
-      [WalletOwnerType.Master]: {
-        [WalletType.Safe]: {
-          [TokenSymbol.ETH]: Number(
-            formatEther(`${memeooorrRequirements.safe}`),
-          ),
-        },
-        [WalletType.EOA]: {
-          [TokenSymbol.ETH]: 0.0125, // TODO: should come from the template
-        },
-      },
-      [WalletOwnerType.Agent]: {
-        [WalletType.Safe]: {
-          [TokenSymbol.ETH]: Number(
-            formatEther(`${memeooorrRequirements.agent}`),
-          ),
-        },
-      },
-    },
     requiresMasterSafesOn: [EvmChainId.Base],
     serviceApi: MemeooorBaseService,
     displayName: 'Agents.fun agent',
@@ -110,29 +57,6 @@ export const AGENT_CONFIG: {
             MODE_TOKEN_CONFIG[TokenSymbol.USDC].decimals,
           ),
         ),
-      },
-    },
-    operatingThresholds: {
-      [WalletOwnerType.Master]: {
-        [WalletType.Safe]: {
-          [TokenSymbol.ETH]: Number(
-            formatEther(
-              `${modiusFundRequirements[ethers.constants.AddressZero].agent}`, // TODO: should be 0.0055, temp fix to avoid low balance alerts until the refund is fixed in the middleware
-            ),
-          ),
-        },
-        [WalletType.EOA]: {
-          [TokenSymbol.ETH]: 0.0002,
-        },
-      },
-      [WalletOwnerType.Agent]: {
-        [WalletType.Safe]: {
-          [TokenSymbol.ETH]: Number(
-            formatEther(
-              `${modiusFundRequirements[ethers.constants.AddressZero].agent}`,
-            ),
-          ),
-        },
       },
     },
     requiresMasterSafesOn: [EvmChainId.Mode],
