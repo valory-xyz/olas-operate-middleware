e9f2370a697b684e5a3ac69f078cee66b2390107
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2025-01-10 19:02:32 +0100

    release: 0.2.0-rc87 (trader + memeooorr)

--- frontend/config/agents.ts
+++ frontend/config/agents.ts
@@ -91,7 +91,7 @@ export const AGENT_CONFIG: {
     displayName: 'Agents.fun agent',
     description:
       'Autonomously post to Twitter, create and trade memecoins, and interact with other agents.',
-    isAgentEnabled: false,
+    isAgentEnabled: true,
   },
   [AgentType.Modius]: {
     name: 'Modius agent',
@@ -138,6 +138,6 @@ export const AGENT_CONFIG: {
     displayName: 'Modius agent',
     description:
       'Invests crypto assets on your behalf and grows your portfolio.',
-    isAgentEnabled: true,
+    isAgentEnabled: false,
   },
 };
