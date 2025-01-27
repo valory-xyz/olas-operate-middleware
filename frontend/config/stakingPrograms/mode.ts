b97acc81e322a0cc9292ce7d9fbd1250c17c651a
truemiller <mlxndrj@gmail.com>
2025-01-27 12:21:20 +0000

    feat: add Modius Optimus Alpha staking program address

--- frontend/config/stakingPrograms/mode.ts
+++ frontend/config/stakingPrograms/mode.ts
@@ -14,6 +14,8 @@ export const MODE_STAKING_PROGRAMS_CONTRACT_ADDRESSES: Record<string, Address> =
   {
     [StakingProgramId.ModiusAlpha]:
       '0x534C0A05B6d4d28d5f3630D6D74857B253cf8332',
+    [StakingProgramId.ModiusOptimusAlpha]:
+      '0x5fc25f50e96857373c64dc0edb1abcbed4587e91',
   };
 
 export const MODE_STAKING_PROGRAMS: StakingProgramMap = {
