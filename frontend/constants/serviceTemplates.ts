80a770529edee9e08b7f8e8d95eb348888d8b262
Atatakai <densatsu@mail.ru>
2025-01-30 16:27:47 +0400

    release: 0.2.0-rc147 (trader + memeooorr)

--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -98,11 +98,11 @@ const AGENTS_FUN_COMMON_TEMPLATE: Pick<
   ServiceTemplate,
   'env_variables' | 'hash' | 'image' | 'description' | 'service_version'
 > = {
-  hash: 'bafybeidstyvnzrvldc5pprhlyal7wikmns2wgdlxtlqar35ujycc4hxa7y',
+  hash: 'bafybeidozytbbt3ktgx67it5kzjpkotzol7oggi233lqtuqupb3tjjn3ty',
   image:
     'https://gateway.autonolas.tech/ipfs/QmQYDGMg8m91QQkTWSSmANs5tZwKrmvUCawXZfXVVWQPcu',
   description: 'Memeooorr @twitter_handle', // should be overwritten with twitter username
-  service_version: 'v0.3.0-alpha6',
+  service_version: 'v0.3.0-alpha7',
   env_variables: {
     BASE_LEDGER_RPC: {
       name: 'Base ledger RPC',
