0eca5a041bbcb480e55448d1142ccd5e0f90f52b
mohandast52 <mohandast52@gmail.com>
2025-01-30 17:04:36 +0530

    feat: enhance CardFlex component with noBorder prop and update usage in AgentSelection and EarlyAccessOnly

--- frontend/components/SetupPage/EarlyAccessOnly.tsx
+++ frontend/components/SetupPage/EarlyAccessOnly.tsx
@@ -1,11 +1,13 @@
 import { IdcardOutlined } from '@ant-design/icons';
-import { Button, Flex, Typography } from 'antd';
+import { Button, Typography } from 'antd';
 import { FC, useCallback } from 'react';
 
 import { COLOR } from '@/constants/colors';
 import { UNICODE_SYMBOLS } from '@/constants/symbols';
 import { SetupScreen } from '@/enums/SetupScreen';
 import { useSetup } from '@/hooks/useSetup';
+
+import { CardFlex } from '../styled/CardFlex';
 
 const { Title, Paragraph } = Typography;
 
@@ -17,12 +19,10 @@ export const EarlyAccessOnly: FC = () => {
   }, [goto]);
 
   return (
-    <Flex
+    <CardFlex
       gap={20}
-      vertical
-      align="center"
-      justify="center"
-      style={{ padding: 48 }}
+      noBorder
+      styles={{ body: { padding: '24px 48px', alignItems: 'center' } }}
     >
       <IdcardOutlined style={{ fontSize: '48px', color: COLOR.TEXT_LIGHT }} />
       <Title level={3} className="m-0">
@@ -43,6 +43,6 @@ export const EarlyAccessOnly: FC = () => {
       <Button type="default" onClick={onGoBack}>
         Go back
       </Button>
-    </Flex>
+    </CardFlex>
   );
 };
