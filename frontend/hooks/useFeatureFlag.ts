ba6c4fdfb76ec852e002f5e75825ca62872d630c
truemiller <mlxndrj@gmail.com>
2025-02-07 13:47:17 +0000

    feat: enable withdraw-funds feature for Memeooorr and Modius agents

--- frontend/hooks/useFeatureFlag.ts
+++ frontend/hooks/useFeatureFlag.ts
@@ -42,7 +42,7 @@ const FEATURES_CONFIG = FeaturesConfigSchema.parse({
   },
   [AgentType.Memeooorr]: {
     'manage-wallet': true,
-    'withdraw-funds': false,
+    'withdraw-funds': true,
     'last-transactions': true,
     'rewards-streak': true,
     'staking-contract-section': true,
@@ -64,7 +64,7 @@ const FEATURES_CONFIG = FeaturesConfigSchema.parse({
   },
   [AgentType.Modius]: {
     'manage-wallet': true,
-    'withdraw-funds': false,
+    'withdraw-funds': true,
     'last-transactions': true,
     'rewards-streak': true,
     'staking-contract-section': true,
