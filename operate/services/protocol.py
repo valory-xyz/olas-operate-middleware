dbe3157402967c5ae27dd1f2d37670ca9de4adc3
nrosa <naiane.rosa@valory.xyz>
2025-02-26 20:28:25 +0000

    Remove agent_id check from staking mechanism

--- operate/services/protocol.py
+++ operate/services/protocol.py
@@ -888,7 +888,7 @@ class OnChainManager(_ChainUtil):
             .load_metadata()
             .set_metadata_fields(description=metadata_description)
             .verify_nft(nft=nft)
-            .verify_service_dependencies(agent_id=agent_id)
+            #.verify_service_dependencies(agent_id=agent_id)
             .publish_metadata()
         )
 
