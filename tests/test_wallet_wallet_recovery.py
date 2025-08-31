9aa0f00ff96cd7b32d0008f84053d7254b81be95
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2025-08-31 11:43:56 +0200

    fix: test

--- tests/test_wallet_wallet_recovery.py
+++ tests/test_wallet_wallet_recovery.py
@@ -19,19 +19,19 @@
 
 """Tests for wallet.wallet_recovery_manager module."""
 
-import os
-import tempfile
+
+# pylint: disable=too-many-locals
+
 import uuid
+from dataclasses import dataclass
 from pathlib import Path
 
 import pytest
-from aea.crypto.base import Crypto
 from aea.helpers.logging import setup_logger
-from aea_ledger_ethereum import EthereumCrypto
-from eth_account.signers.local import LocalAccount
-from web3 import Account
 
 from operate.cli import OperateApp
+from operate.constants import KEYS_DIR
+from operate.keys import KeysManager
 from operate.operate_types import Chain, LedgerType
 from operate.utils.gnosis import add_owner, remove_owner, swap_owner
 from operate.wallet.master import MasterWalletManager
@@ -50,34 +50,76 @@ LEDGER_TO_CHAINS = {LedgerType.ETHEREUM: [Chain.GNOSIS, Chain.BASE]}
 LOGGER = setup_logger(name="operate-test")
 
 
-# TODO decide if use KeysManager method instead.
-def create_crypto(ledger_type: LedgerType, private_key: str) -> Crypto:
-    """create_crypto"""
-    with tempfile.NamedTemporaryFile(
-        mode="w",
-        suffix=".txt",
-        delete=False,  # Handle cleanup manually
-    ) as temp_file:
-        temp_file_name = temp_file.name
-        temp_file.write(private_key)
-        temp_file.flush()
-        temp_file.close()  # Close the file before reading
+@dataclass
+class TestEnv:
+    """Test environment."""
 
-        # Set proper file permissions (readable by owner only)
-        os.chmod(temp_file_name, 0o600)
-        crypto = EthereumCrypto(private_key_path=temp_file_name)
+    tmp_path: Path
+    password: str
+    operate: OperateApp
+    wallet_manager: MasterWalletManager
+    keys_manager: KeysManager
+    backup_wallet: str
+    backup_wallet2: str
 
-        try:
-            with open(temp_file_name, "r+", encoding="utf-8") as f:
-                f.seek(0)
-                f.write("\0" * len(private_key))
-                f.flush()
-                f.close()
-            os.unlink(temp_file_name)  # Clean up the temporary file
-        except OSError as e:
-            raise RuntimeError(f"Failed to delete temp file {temp_file.name}") from e
 
-    return crypto
+def _create_wallets(wallet_manager: MasterWalletManager) -> None:
+    for ledger_type in LEDGER_TO_CHAINS:
+        wallet_manager.create(ledger_type=ledger_type)
+
+
+def _create_safes(wallet_manager: MasterWalletManager, backup_owner: str) -> None:
+    for ledger_type, chains in LEDGER_TO_CHAINS.items():
+        wallet = wallet_manager.load(ledger_type=ledger_type)
+        for chain in chains:
+            tenderly_add_balance(chain, wallet.address)
+            tenderly_add_balance(chain, backup_owner)
+            wallet.create_safe(
+                chain=chain,
+                backup_owner=backup_owner,
+            )
+
+
+@pytest.fixture
+def test_env(tmp_path: Path, password: str) -> TestEnv:
+    """Sets up a test environment."""
+    operate = OperateApp(
+        home=tmp_path / OPERATE_TEST,
+    )
+    operate.setup()
+    operate.create_user_account(password=password)
+    operate.password = password
+    wallet_manager = operate.wallet_manager
+    wallet_manager.setup()
+    keys_manager = KeysManager(
+        path=operate._path / KEYS_DIR,  # pylint: disable=protected-access
+        logger=LOGGER,
+    )
+    backup_wallet = keys_manager.create()
+    backup_wallet2 = keys_manager.create()
+
+    assert backup_wallet != backup_wallet2
+
+    _create_wallets(wallet_manager=wallet_manager)
+    _create_safes(
+        wallet_manager=wallet_manager,
+        backup_owner=backup_wallet,
+    )
+
+    # Logout
+    operate = OperateApp(
+        home=tmp_path / OPERATE_TEST,
+    )
+
+    return TestEnv(
+        tmp_path=tmp_path,
+        password=password,
+        operate=operate,
+        wallet_manager=wallet_manager,
+        keys_manager=keys_manager,
+        backup_wallet=backup_wallet,
+        backup_wallet2=backup_wallet2,
+    )
 
 
 class TestWalletRecovery:
@@ -103,48 +145,16 @@ class TestWalletRecovery:
             assert old_wallet.safes == wallet.safes
             assert old_wallet.safe_chains == wallet.safe_chains
 
-    @staticmethod
-    def _create_wallets(wallet_manager: MasterWalletManager) -> None:
-        for ledger_type in LEDGER_TO_CHAINS:
-            wallet_manager.create(ledger_type=ledger_type)
-
-    @staticmethod
-    def _create_safes(wallet_manager: MasterWalletManager, backup_owner: str) -> None:
-        for ledger_type, chains in LEDGER_TO_CHAINS.items():
-            wallet = wallet_manager.load(ledger_type=ledger_type)
-            for chain in chains:
-                tenderly_add_balance(chain, wallet.address)
-                tenderly_add_balance(chain, backup_owner)
-                wallet.create_safe(
-                    chain=chain,
-                    backup_owner=backup_owner,
-                )
-
     def test_normal_flow(
         self,
-        tmp_path: Path,
-        password: str,
+        test_env: TestEnv,
     ) -> None:
         """test_normal_flow"""
-        operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
-        )
-        operate.setup()
-        operate.create_user_account(password=password)
-        operate.password = password
-        wallet_manager = operate.wallet_manager
-        wallet_manager.setup()
-        backup_wallet: LocalAccount = Account().create()
-        TestWalletRecovery._create_wallets(wallet_manager=wallet_manager)
-        TestWalletRecovery._create_safes(
-            wallet_manager=wallet_manager,
-            backup_owner=backup_wallet.address,
-        )
-
-        # Logout
-        operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
-        )
+        operate = test_env.operate
+        wallet_manager = test_env.wallet_manager
+        keys_manager = test_env.keys_manager
+        backup_wallet = test_env.backup_wallet
+        password = test_env.password
 
         # Recovery step 1
         new_password = password[::-1]
@@ -174,11 +184,7 @@ class TestWalletRecovery:
 
         # Swap safe owners using backup wallet
         for item in step_1_output["wallets"]:
-            crypto = create_crypto(
-                ledger_type=LedgerType(item["current_wallet"]["ledger_type"]),
-                private_key=backup_wallet.key.hex(),
-            )
-
+            crypto = keys_manager.get_crypto_instance(backup_wallet)
             for ledger_type, chains in LEDGER_TO_CHAINS.items():
                 wallet = wallet_manager.load(ledger_type=ledger_type)
                 for chain in chains:
@@ -199,7 +205,7 @@ class TestWalletRecovery:
 
         # Test that recovery was successful
         operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
+            home=test_env.tmp_path / OPERATE_TEST,
         )
         assert operate.user_account is not None
         assert operate.user_account.is_valid(new_password)
@@ -221,7 +227,7 @@ class TestWalletRecovery:
 
         # Attempt to do a recovery with the same bundle will result in error
         operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
+            home=test_env.tmp_path / OPERATE_TEST,
         )
         with pytest.raises(
             ValueError, match=f"Recovery bundle {bundle_id} has been executed already."
@@ -232,29 +238,14 @@ class TestWalletRecovery:
 
     def test_resumed_flow(
         self,
-        tmp_path: Path,
-        password: str,
+        test_env: TestEnv,
     ) -> None:
-        """test_incomplete_flow"""
-        operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
-        )
-        operate.setup()
-        operate.create_user_account(password=password)
-        operate.password = password
-        wallet_manager = operate.wallet_manager
-        wallet_manager.setup()
-        backup_wallet: LocalAccount = Account().create()
-        TestWalletRecovery._create_wallets(wallet_manager=wallet_manager)
-        TestWalletRecovery._create_safes(
-            wallet_manager=wallet_manager,
-            backup_owner=backup_wallet.address,
-        )
-
-        # Logout
-        operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
-        )
+        """test_resumed_flow"""
+        operate = test_env.operate
+        wallet_manager = test_env.wallet_manager
+        keys_manager = test_env.keys_manager
+        backup_wallet = test_env.backup_wallet
+        password = test_env.password
 
         # Recovery step 1
         new_password = password[::-1]
@@ -292,11 +283,7 @@ class TestWalletRecovery:
             ledger_to_chains_2[ledger] = chains[mid:]
 
         for item in step_1_output["wallets"]:
-            crypto = create_crypto(
-                ledger_type=LedgerType(item["current_wallet"]["ledger_type"]),
-                private_key=backup_wallet.key.hex(),
-            )
-
+            crypto = keys_manager.get_crypto_instance(backup_wallet)
             for ledger_type, chains in ledger_to_chains_1.items():
                 wallet = wallet_manager.load(ledger_type=ledger_type)
                 for chain in chains:
@@ -318,11 +305,7 @@ class TestWalletRecovery:
 
         # Resume swapping safe owners using backup wallet
         for item in step_1_output["wallets"]:
-            crypto = create_crypto(
-                ledger_type=LedgerType(item["current_wallet"]["ledger_type"]),
-                private_key=backup_wallet.key.hex(),
-            )
-
+            crypto = keys_manager.get_crypto_instance(backup_wallet)
             for ledger_type, chains in ledger_to_chains_2.items():
                 wallet = wallet_manager.load(ledger_type=ledger_type)
                 for chain in chains:
@@ -343,7 +326,7 @@ class TestWalletRecovery:
 
         # Test that recovery was successful
         operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
+            home=test_env.tmp_path / OPERATE_TEST,
         )
         assert operate.user_account is not None
         assert operate.user_account.is_valid(new_password)
@@ -366,26 +349,19 @@ class TestWalletRecovery:
     @pytest.mark.parametrize("raise_if_inconsistent_owners", [True, False])
     def test_exceptions(
         self,
-        tmp_path: Path,
-        password: str,
+        test_env: TestEnv,
         raise_if_inconsistent_owners: bool,
     ) -> None:
         """test_exceptions"""
-        operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
-        )
-        operate.setup()
-        operate.create_user_account(password=password)
+        operate = test_env.operate
+        wallet_manager = test_env.wallet_manager
+        keys_manager = test_env.keys_manager
+        backup_wallet = test_env.backup_wallet
+        backup_wallet2 = test_env.backup_wallet2
+        password = test_env.password
+
+        # Log in
         operate.password = password
-        wallet_manager = operate.wallet_manager
-        wallet_manager.setup()
-        backup_wallet: LocalAccount = Account().create()
-        backup_wallet2: LocalAccount = Account().create()
-        TestWalletRecovery._create_wallets(wallet_manager=wallet_manager)
-        TestWalletRecovery._create_safes(
-            wallet_manager=wallet_manager,
-            backup_owner=backup_wallet.address,
-        )
 
         new_password = password[::-1]
         with pytest.raises(
@@ -398,7 +374,7 @@ class TestWalletRecovery:
 
         # Logout
         operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
+            home=test_env.tmp_path / OPERATE_TEST,
         )
 
         with pytest.raises(
@@ -431,7 +407,7 @@ class TestWalletRecovery:
 
         # Logout
         operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
+            home=test_env.tmp_path / OPERATE_TEST,
         )
 
         random_bundle_id = f"{RECOVERY_BUNDLE_PREFIX}{str(uuid.uuid4())}"
@@ -466,11 +442,7 @@ class TestWalletRecovery:
 
         # Add safe owners using backup wallet
         for item in step_1_output["wallets"]:
-            crypto = create_crypto(
-                ledger_type=LedgerType(item["current_wallet"]["ledger_type"]),
-                private_key=backup_wallet.key.hex(),
-            )
-
+            crypto = keys_manager.get_crypto_instance(backup_wallet)
             for ledger_type, chains in LEDGER_TO_CHAINS.items():
                 wallet = wallet_manager.load(ledger_type=ledger_type)
                 for chain in chains:
@@ -492,11 +464,7 @@ class TestWalletRecovery:
         if raise_if_inconsistent_owners:
             # Remove old MasterEOA
             for item in step_1_output["wallets"]:
-                crypto = create_crypto(
-                    ledger_type=LedgerType(item["current_wallet"]["ledger_type"]),
-                    private_key=backup_wallet.key.hex(),
-                )
-
+                crypto = keys_manager.get_crypto_instance(backup_wallet)
                 for ledger_type, chains in LEDGER_TO_CHAINS.items():
                     wallet = wallet_manager.load(ledger_type=ledger_type)
                     for chain in chains:
@@ -519,11 +487,7 @@ class TestWalletRecovery:
 
             # Use a different backup owner for half of the chains
             for item in step_1_output["wallets"]:
-                crypto = create_crypto(
-                    ledger_type=LedgerType(item["current_wallet"]["ledger_type"]),
-                    private_key=backup_wallet.key.hex(),
-                )
-
+                crypto = keys_manager.get_crypto_instance(backup_wallet)
                 for ledger_type, chains in ledger_to_chains_1.items():
                     wallet = wallet_manager.load(ledger_type=ledger_type)
                     for chain in chains:
@@ -532,8 +496,8 @@ class TestWalletRecovery:
                             ledger_api=ledger_api,
                             crypto=crypto,
                             safe=item["current_wallet"]["safes"][chain.value],
-                            old_owner=backup_wallet.address,
-                            new_owner=backup_wallet2.address,
+                            old_owner=backup_wallet,
+                            new_owner=backup_wallet2,
                         )
 
             with pytest.raises(
@@ -548,22 +512,18 @@ class TestWalletRecovery:
 
             # Revert original backup owner
             for item in step_1_output["wallets"]:
-                crypto = create_crypto(
-                    ledger_type=LedgerType(item["current_wallet"]["ledger_type"]),
-                    private_key=backup_wallet2.key.hex(),
-                )
-
+                crypto = keys_manager.get_crypto_instance(backup_wallet2)
                 for ledger_type, chains in ledger_to_chains_1.items():
                     wallet = wallet_manager.load(ledger_type=ledger_type)
                     for chain in chains:
-                        tenderly_add_balance(chain, backup_wallet2.address)
+                        tenderly_add_balance(chain, backup_wallet2)
                         ledger_api = wallet.ledger_api(chain)
                         swap_owner(
                             ledger_api=ledger_api,
                             crypto=crypto,
                             safe=item["current_wallet"]["safes"][chain.value],
-                            old_owner=backup_wallet2.address,
-                            new_owner=backup_wallet.address,
+                            old_owner=backup_wallet2,
+                            new_owner=backup_wallet,
                         )
 
         # Recovery step 2
@@ -575,7 +535,7 @@ class TestWalletRecovery:
 
         # Test that recovery was successful
         operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
+            home=test_env.tmp_path / OPERATE_TEST,
         )
         assert operate.user_account is not None
         assert operate.user_account.is_valid(new_password)
@@ -597,7 +557,7 @@ class TestWalletRecovery:
 
         # Attempt to do a recovery with the same bundle will result in error
         operate = OperateApp(
-            home=tmp_path / OPERATE_TEST,
+            home=test_env.tmp_path / OPERATE_TEST,
         )
         with pytest.raises(
             ValueError, match=f"Recovery bundle {bundle_id} has been executed already."
