bab020ddbc2ef34c60c2a910a782ef4bfe727770
PettBro <bptteam@proton.me>
2025-12-01 20:36:55 +0000

    added pett_ai to QS

--- operate/quickstart/run_service.py
+++ operate/quickstart/run_service.py
@@ -104,6 +104,7 @@ QS_STAKING_PROGRAMS: t.Dict[Chain, t.Dict[str, str]] = {
         "quickstart_beta_mech_marketplace_expert_10": "trader",
         "mech_marketplace": "mech",
         "marketplace_supply_alpha": "mech",
+        "pett_ai_agent": "mech",
     },
     Chain.OPTIMISM: {
         "optimus_alpha_2": "optimus",
